cd7c62938bdd78ffcd38c76c8c5f2125b08a5eb5
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2023-11-06 23:08:12 -0500

    More sounds

--- mods/sounds/init.lua
+++ mods/sounds/init.lua
@@ -1,3 +1,5 @@
+
+
 println(
 "Loading sounds.\n",
 "Credits:\n",
@@ -5,5 +7,8 @@ println(
 "CC BY 4.0 can be read here: https://creativecommons.org/licenses/by/4.0/",
 "grass_step_7: https://freesound.org/people/spycrah/sounds/535220/ CC BY 4.0",
 "grass_step_8: https://freesound.org/people/spycrah/sounds/535222/ CC BY 4.0",
-
-"")
+"CC BY 3.0 can be read here: https://creativecommons.org/licenses/by/3.0/",
+"dirt_step_(1-4): https://freesound.org/people/kMoon/sounds/167157/ CC BY 3.0",
+"sand_step_(1-8): https://freesound.org/people/worthahep88/sounds/319208/ CC0",
+"gravel_step_(1-6): https://freesound.org/people/SpliceSound/sounds/218304/ CC0",
+"wool_step_(1-6): https://freesound.org/people/Adielees9/sounds/331167/ CC0")
