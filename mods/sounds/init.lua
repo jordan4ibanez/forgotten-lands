309dd633cdcc8e3559b6ae7dcec5b8a6b1a30cec
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2023-11-06 22:56:49 -0500

    Bring in some sounds

---
+++ mods/sounds/init.lua
@@ -0,0 +1,9 @@
+println(
+"Loading sounds.\n",
+"Credits:\n",
+"grass_step_(1-6): https://freesound.org/people/Yuval/sounds/189640/ CC0",
+"CC BY 4.0 can be read here: https://creativecommons.org/licenses/by/4.0/",
+"grass_step_7: https://freesound.org/people/spycrah/sounds/535220/ CC BY 4.0",
+"grass_step_8: https://freesound.org/people/spycrah/sounds/535222/ CC BY 4.0",
+
+"")
