298afc0607f30dec84051dcad05f036c96f82095
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2023-11-08 04:37:05 -0500

    Fix drops

--- mods/item_drop/init.lua
+++ mods/item_drop/init.lua
@@ -1,6 +1,6 @@
 local _tl_compat; if (tonumber((_VERSION or ''):match('[%d.]*$')) or 0) < 5.3 then local p, m = pcall(require, 'compat53.module'); if p then _tl_compat = m end end; local ipairs = _tl_compat and _tl_compat.ipairs or ipairs
 
-function minetest.handle_node_drops(pos, drops, digger)
+function minetest.handle_node_drops(pos, drops, _digger)
 
 
 
