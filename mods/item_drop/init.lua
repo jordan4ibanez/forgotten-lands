4634bf8bbb3b54d0dfd673ee3cd6b1d8df8e0784
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2023-11-07 21:38:54 -0500

    Add the basis of item drop

--- mods/item_drop/init.lua
+++ mods/item_drop/init.lua
@@ -1,9 +1,6 @@
-local _tl_compat; if (tonumber((_VERSION or ''):match('[%d.]*$')) or 0) < 5.3 then local p, m = pcall(require, 'compat53.module'); if p then _tl_compat = m end end; local ipairs = _tl_compat and _tl_compat.ipairs or ipairs; function minetest.handle_node_drops(pos, drops, digger)
-   print("test", pos.x, pos.y, pos.z)
-   minetest.add_particle({
-      pos = pos,
-      texture = "default_dirt.png",
-   })
+local _tl_compat; if (tonumber((_VERSION or ''):match('[%d.]*$')) or 0) < 5.3 then local p, m = pcall(require, 'compat53.module'); if p then _tl_compat = m end end; local ipairs = _tl_compat and _tl_compat.ipairs or ipairs
+
+function minetest.handle_node_drops(pos, drops, digger)
 
 
 
@@ -11,12 +8,21 @@ local _tl_compat; if (tonumber((_VERSION or ''):match('[%d.]*$')) or 0) < 5.3 th
 
 
 
-   print(dump(drops))
+
+
+
+
 
    for _, drop in ipairs(drops) do
       local item = minetest.add_item(pos, drop)
       if item then
-         (item):add_velocity(vector.new())
+         (item):add_velocity(
+         vector.random(
+         -1, 1,
+         1, 2,
+         -1, 1))
+
+
       end
    end
 end
