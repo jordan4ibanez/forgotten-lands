912d686f92a9770e65f188879e1a5cf6523acdd0
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2023-11-12 21:14:29 -0500

    UH OH

---
+++ mods/utility/init.ts
@@ -0,0 +1,7 @@
+import { MtVec3 } from "@repcomm/mt-api";
+
+function generateSchematic(size: MtVec3, keys: Map<String, String>, forcePlace: Map<String, Boolean>, ySliceProb: Array<number>) {
+  // var newSchematic = {
+  //   size = size
+  // }
+}
