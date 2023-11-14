483c82eb9e7b8f3d55ec707d50ab23ea8a80c3e9
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2023-11-14 05:03:55 -0500

    Translate blocks

--- mods/blocks/init.lua
+++ mods/blocks/init.lua
@@ -1,1 +1,29 @@
 Blocks = Blocks or ({})
+do
+    local sounds = Sounds
+    do
+        local i = 1
+        while i <= 5 do
+            minetest.register_node(
+                ":tall_grass_" .. tostring(i),
+                {
+                    drawtype = Drawtype.plantlike,
+                    walkable = false,
+                    waving = 1,
+                    paramtype = ParamType1.light,
+                    paramtype2 = ParamType2.degrotate,
+                    tiles = {("default_grass_" .. tostring(i)) .. ".png"},
+                    buildable_to = true,
+                    sounds = sounds.plant(),
+                    groups = {break_instant = 1},
+                    drop = ""
+                }
+            )
+            i = i + 1
+        end
+    end
+    local modDirectory = minetest.get_modpath("blocks")
+    for ____, file in ipairs({"normal"}) do
+        dofile(((modDirectory .. "/") .. file) .. ".lua")
+    end
+end
--- mods/blocks/init.ts
+++ mods/blocks/init.ts
@@ -1,3 +1,25 @@
 namespace Blocks {
-  
+  const sounds = Sounds;
+
+  for (let i = 1; i <= 5; i++) {
+    minetest.register_node(":tall_grass_" + i, {
+      drawtype: Drawtype.plantlike,
+      walkable: false,
+      waving: 1,
+      paramtype: ParamType1.light,
+      paramtype2: ParamType2.degrotate,
+      tiles: ["default_grass_" + i + ".png"],
+      buildable_to: true,
+      sounds: sounds.plant(),
+      groups: {
+        break_instant: 1
+      },
+      drop: ""
+    })
+  }
+
+  const modDirectory = minetest.get_modpath("blocks")
+  for (const file of ["normal"]) {
+    dofile(modDirectory + "/" + file + ".lua")
+  }
 }
