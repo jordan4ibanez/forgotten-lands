aff00aa608ce1080ee6cc6fb44e89725574b015d
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2023-11-14 01:53:39 -0500

    translate a bunch of item entity

--- mods/item_entity/init.lua
+++ mods/item_entity/init.lua
@@ -9,6 +9,8 @@ end
 do
     local println = utility.println
     local fakeRef = utility.fakeRef
+    local timeToLive = tonumber(minetest.settings:get("item_entity_ttl")) or 900
+    local gravity = tonumber(minetest.settings:get("movement_gravity")) or -9.81
     local ItemEntity = __TS__Class()
     ItemEntity.name = "ItemEntity"
     function ItemEntity.prototype.____constructor(self)
@@ -18,9 +20,18 @@ do
         self.movingState = true
         self.physicalState = true
         self.age = 0
+        self.beingForcedOut = false
         self.forceOut = vector.create()
         self.forceOutStart = vector.create()
         self.droppedBy = ""
+        self._collisionBox = {
+            0,
+            0,
+            0,
+            0,
+            0,
+            0
+        }
         self.collected = false
         self.initial_properties = {
             hp_max = 1,
@@ -51,7 +62,7 @@ do
         local size = 0.21
         local def = minetest.registered_items[itemName]
         local glow = def and def.light_source and math.floor(def.light_source / 2 + 0.5)
-        local sizeBias = 0.001 * math.random()
+        local bias = 0.001 * math.random()
         local c = {
             -size,
             -size,
@@ -60,9 +71,177 @@ do
             size,
             size
         }
-        self.object:set_properties({is_visible = true})
+        self.object:set_properties({
+            is_visible = true,
+            visual = EntityVisual.wielditem,
+            textures = {itemName},
+            visual_size = vector.create2d(size + bias, size + bias),
+            collisionbox = c,
+            wield_item = self.itemString,
+            glow = glow,
+            infotext = stack:get_description()
+        })
+        self._collisionBox = c
+    end
+    function ItemEntity.prototype.get_staticdata(self)
+        return minetest.serialize({itemString = self.itemString, age = self.age, droppedBy = self.droppedBy})
     end
     function ItemEntity.prototype.on_activate(self, staticData, delta)
+        if string.sub(
+            staticData,
+            1,
+            string.len("return")
+        ) == "return" then
+            local data = minetest.deserialize(staticData)
+            if data and type(data) == "table" then
+                self.itemString = data.itemString
+                self.age = data.age
+                self.droppedBy = data.droppedBy
+            end
+        else
+            self.itemString = staticData
+        end
+    end
+    function ItemEntity.prototype.enablePhysics(self)
+        if self.physicalState then
+            return
+        end
+        self.physicalState = true
+        self.object:set_properties({physical = true})
+        self.object:set_velocity(vector.create())
+        self.object:set_acceleration(vector.create(0, -gravity, 0))
+    end
+    function ItemEntity.prototype.disablePhysics(self)
+        if not self.physicalState then
+            return
+        end
+        self.physicalState = false
+        self.object:set_properties({physical = true})
+        self.object:set_velocity(vector.create())
+        local ____ = self.object.set_acceleration
+    end
+    function ItemEntity.prototype.tickAge(self, delta)
+        self.age = self.age + delta
+        if timeToLive > 0 and self.age > timeToLive then
+            self.itemString = ""
+            self.object:remove()
+            return true
+        end
+        return false
+    end
+    function ItemEntity.prototype.checkOutOfBounds(self, node)
+        if node and node.name == "ignore" then
+            self.itemString = ""
+            self.object:remove()
+            return true
+        end
+        return false
+    end
+    function ItemEntity.prototype.slipCheck(self, delta, def, node)
+        if not def then
+            return false
+        end
+        local slippery = minetest.get_item_group(node.name, "slippery")
+        local vel = self.object:get_velocity()
+        if slippery ~= 0 and (math.abs(vel.x) > 0.1 or math.abs(vel.z)) then
+            local factor = math.min(4 / (slippery + 4) * delta, 1)
+            self.object:set_velocity(vector.create(vel.x * (1 - factor), 0, vel.z * (1 - factor)))
+            return true
+        end
+        return false
+    end
+    function ItemEntity.prototype.forceOutCheck(self, pos)
+        if not self.beingForcedOut then
+            return false
+        end
+        local c = self._collisionBox
+        local s = self.forceOutStart
+        local f = self.forceOut
+        local okay = f.x > 0 and pos.x + c[2] > s.x + 0.5 or f.y > 0 and pos.y + c[3] > s.y + 0.5 or f.z > 0 and pos.z + c[4] > s.z + 0.5 or f.x < 0 and pos.x + c[5] < s.x - 0.5 or f.z < 0 and pos.z + c[7] < s.z - 0.5
+        if not okay then
+            return false
+        end
+        self.beingForcedOut = false
+        self:enablePhysics()
+        return true
+    end
+    function ItemEntity.prototype.pollPlayers(self, pos)
+        if self.collected then
+            return
+        end
+        if self.age < 1.5 then
+            return
+        end
+        for ____, player in ipairs(minetest.get_connected_players()) do
+            do
+                local playerPos = player:get_pos()
+                if vector.distance(pos, playerPos) > 3 then
+                    goto __continue28
+                end
+                if vector.distance2d(pos, playerPos) > 1.5 then
+                    goto __continue28
+                end
+                if playerPos.y - pos.y > 0.05 then
+                    goto __continue28
+                end
+                local inv = player:get_inventory()
+                if not inv then
+                    goto __continue28
+                end
+                if not inv:room_for_item("main", self.itemString) then
+                    goto __continue28
+                end
+                inv:add_item("main", self.itemString)
+                self:disablePhysics()
+                self.object:move_to(playerPos, true)
+                self.age = 0
+                self.collected = true
+                addMagnetPop(player)
+            end
+            ::__continue28::
+        end
+    end
+    function ItemEntity.prototype.collectionCleanup(self, delta)
+        self.age = self.age + delta
+        if self.age < 0.2 then
+            return
+        end
+        self.object:remove()
+    end
+    function ItemEntity.prototype.unstuckSelf(self, pos, isStuck)
+        if not isStuck then
+            return
+        end
+        local shootDir = (function()
+            local order = {
+                vector.create(1, 0, 0),
+                vector.create(-1, 0, 0),
+                vector.create(0, 0, 1),
+                vector.create(0, 0, -1)
+            }
+            for ____, direction in ipairs(order) do
+                local cNode = minetest.get_node(vector.add(pos, direction)).name
+                local cDef = minetest.registered_nodes[cNode] or nil
+                if cNode ~= "ignore" and cDef and cDef.walkable == false then
+                    return direction
+                end
+            end
+            local lastTry = vector.create(0, 1, 0)
+            local cNode = minetest.get_node(vector.add(pos, lastTry)).name
+            if cNode ~= "ignore" then
+                return lastTry
+            end
+            return nil
+        end)()
+        if not shootDir then
+            return
+        end
+        local newVelocity = vector.multiply(shootDir, 3)
+        self:disablePhysics()
+        self.object:set_velocity(newVelocity)
+        self.forceOut = newVelocity
+        self.forceOutStart = vector.round(pos)
+        self.beingForcedOut = true
     end
     function ItemEntity.prototype.on_step(self, delta, moveResult)
     end
--- mods/item_entity/init.ts
+++ mods/item_entity/init.ts
@@ -1,6 +1,8 @@
 {
   const println = utility.println;
   const fakeRef = utility.fakeRef;
+  const timeToLive = tonumber(minetest.settings.get("item_entity_ttl")) || 900
+  const gravity = tonumber(minetest.settings.get("movement_gravity")) || -9.81
 
   class ItemEntity implements LuaEntity {
     name = "__builtin:item"
@@ -9,9 +11,11 @@
     movingState = true
     physicalState = true
     age = 0
+    beingForcedOut = false
     forceOut = vector.create()
     forceOutStart = vector.create()
     droppedBy = ""
+    _collisionBox = [0,0,0,0,0,0]
     // Magnet components.
     collected = false
 
@@ -41,23 +45,193 @@
       const glow = def && def.light_source && math.floor(def.light_source / 2 + 0.5)
       
       // Small random bias to counter Z-fighting.
-      const sizeBias = 1e-3 * math.random()
+      const bias = 1e-3 * math.random()
 
       const c = [-size, -size, -size, size, size, size]
 
       this.object.set_properties({
-        is_visible: true
+        is_visible: true,
+        visual: EntityVisual.wielditem,
+        textures: [itemName],
+        visual_size: vector.create2d(size + bias, size + bias),
+        collisionbox: c,
+        wield_item: this.itemString,
+        glow: glow,
+        infotext: stack.get_description()
       })
 
+      // Cache collisionbox for usage in on_step
+      this._collisionBox = c
+    }
 
-
-
+    get_staticdata(): string {
+      return minetest.serialize({
+        itemString: this.itemString,
+        age: this.age,
+        droppedBy: this.droppedBy
+      })
     }
 
     on_activate(staticData: string, delta: number): void {
-
+      if (string.sub(staticData, 1, string.len("return")) == "return") {
+        const data = minetest.deserialize(staticData)
+        if (data && type(data) == "table") {
+          this.itemString = data.itemString
+          this.age = data.age
+          this.droppedBy = data.droppedBy 
+        }
+      } else {
+        this.itemString = staticData
+      }
     }
+
+    enablePhysics(): void {
+      if (this.physicalState) return
+      this.physicalState = true
+      this.object.set_properties({physical: true})
+      this.object.set_velocity(vector.create())
+      this.object.set_acceleration(vector.create(0,-gravity,0))
+    }
+
+    disablePhysics(): void {
+      if (!this.physicalState) return
+      this.physicalState = false
+      this.object.set_properties({physical: true})
+      this.object.set_velocity(vector.create())
+      this.object.set_acceleration
+    }
+
+    tickAge(delta: number): boolean {
+      this.age += delta
+      if (timeToLive > 0 && this.age > timeToLive) {
+        this.itemString = ""
+        this.object.remove()
+        return true
+      }
+      return false
+    }
+
+    checkOutOfBounds(node: NodeTable): boolean {
+      // Delete if in an ignore node.
+      if (node && node.name == "ignore") {
+        this.itemString = ""
+        this.object.remove()
+        return true
+      }
+      return false
+    }
+
+    slipCheck(delta: number, def: NodeDefinition, node: NodeTable): boolean {
+      if (!def) return false
+      const slippery = minetest.get_item_group(node.name, "slippery")
+      const vel = this.object.get_velocity()
+      // todo: check this with math.sign(vel.x) etc
+      if (slippery != 0 && (math.abs(vel.x) > 0.1 || math.abs(vel.z))) {
+        // Horizontal deceleration.
+        const factor = math.min(4 / (slippery + 4) * delta, 1)
+        this.object.set_velocity(vector.create(
+          vel.x * (1 - factor),
+          0,
+          vel.z * (1 - factor)
+        ))
+        return true
+      }
+      return false
+    }
+
+    forceOutCheck(pos: Vec3): boolean {
+      if (!this.beingForcedOut) return false
+      /*
+      This code runs after the entity got a push from the isStuck() code.
+      It makes sure the entity is entirely outside the solid node.
+      */
+      const c = this._collisionBox
+      const s = this.forceOutStart
+      const f = this.forceOut
+      const okay = (f.x > 0 && pos.x + c[1] > s.x + 0.5) ||
+        (f.y > 0 && pos.y + c[2] > s.y + 0.5) ||
+        (f.z > 0 && pos.z + c[3] > s.z + 0.5) ||
+        (f.x < 0 && pos.x + c[4] < s.x - 0.5) ||
+        (f.z < 0 && pos.z + c[6] < s.z - 0.5)
+
+      if (!okay) return false
+
+      // Item was successfully forced out.
+      this.beingForcedOut = false
+      this.enablePhysics()
+      return true
+    }
+
+    pollPlayers(pos: Vec3): void {
+      if (this.collected) return
+      if (this.age < 1.5) return
+
+      for (const player of minetest.get_connected_players()) {
+        const playerPos = player.get_pos()
+        if (vector.distance(pos, playerPos) > 3) continue
+        if (vector.distance2d(pos, playerPos) > 1.5) continue
+        if (playerPos.y - pos.y > 0.05) continue
+        
+        const inv = player.get_inventory()
+        if (!inv) continue
+        if (!inv.room_for_item("main", this.itemString)) continue
+
+        inv.add_item("main", this.itemString)
+
+        //! fixme: Have a function specifically made to magnetize towards the player because move_to is awful
+        this.disablePhysics()
+        this.object.move_to(playerPos, true)
+        this.age = 0
+        this.collected = true
+        addMagnetPop(player)
+      }
+    }
+
+    collectionCleanup(delta: number): void {
+      this.age += delta
+      if (this.age < 0.2) return
+      this.object.remove()
+    }
+
+    unstuckSelf(pos: Vec3, isStuck: boolean) {
+      if (!isStuck) return
+
+      let shootDir: Vec3 | null = (() => {
+        //! fixme: this should be a cached single instance. Why is this getting created every time?
+        const order = [vector.create(1,0,0),vector.create(-1,0,0),vector.create(0,0,1),vector.create(0,0,-1),]
+        for (const direction of order) {
+          const cNode = minetest.get_node(vector.add(pos, direction)).name
+          const cDef = minetest.registered_nodes[cNode] || null
+          if (cNode != "ignore" && cDef && cDef.walkable == false) {
+            return direction
+          }
+        }
+
+        // Iteration failed on X and Z axis, only way left is to go up.
+        const lastTry = vector.create(0,1,0)
+        const cNode = minetest.get_node(vector.add(pos, lastTry)).name
+        if (cNode != "ignore") {
+          return lastTry
+        }
+        // Everything failed.
+        return null
+      })();
+
+      // Everything failed, give up.
+      if (!shootDir) return
+
+      const newVelocity = vector.multiply(shootDir, 3)
+      this.disablePhysics()
+      this.object.set_velocity(newVelocity)
+
+      this.forceOut = newVelocity
+      this.forceOutStart = vector.round(pos)
+      this.beingForcedOut = true
+      
+    }
+
     on_step(delta: number, moveResult: MoveResult): void {
+
     }
   }
 
