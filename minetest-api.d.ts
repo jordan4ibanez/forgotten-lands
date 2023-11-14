4dbcc2fcd1fe9cee2bed9c7564c60b94801a5f9d
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2023-11-14 05:10:30 -0500

    Update minetest-api.d.ts

--- minetest-api.d.ts
+++ minetest-api.d.ts
@@ -33,7 +33,7 @@ export interface minetest {
   register_node(nodeName: string, definition: NodeDefinition): void
   register_craftitem(craftItemName: string, definition: ItemDefinition): void
   register_tool(toolName: string, definition: ItemDefinition): void
-  override_item(itemName: string, {any : any}): void
+  override_item(itemName: string, definition: ItemDefinition): void
   unregister_item(itemName: string): void
   register_entity(entityName: string, definition: LuaEntity): void
   register_abm(abm: ABMDefinition): void
