01ad419b55bde6082e26490feb83e4645046e87d
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2023-11-14 06:55:08 -0500

    I have a different idea

--- mods/item_drop/init.ts
+++ mods/item_drop/init.ts
@@ -2,79 +2,7 @@ namespace ItemDrop {
   // Namespace magic.
   type ItemEntity = BuiltinEntity.ItemEntity;
 
-  function extractNodeGroups(position: Vec3): {[id: string] : number} | null {
-    // If you dig a node, it should never be ignore. But it might be.
-    const node = minetest.get_node_or_nil(position)
-    if (!node) return null
-    if (node.name == "ignore") return null
-    const def = minetest.registered_nodes[node.name]
-    // Something has gone seriously wrong, we'll role with it.
-    if (!def) {
-      // But we're going going to notify.
-      minetest.log(LogLevel.warning, `Tried to get an UNKNOWN node! ${node.name} does not exist!`)
-      return null
-    }
-    const groups = def.groups
-    if (!groups) {
-      // Again, this is an error. If no groups are defined there's a problem with the node definition.
-      minetest.log(LogLevel.warning, `Node ${node.name} has no defined groups!`)
-      return null
-    }
-    return groups
-  }
-
-  function extractWieldGroups(digger: ObjectRef) {
-
-    // A mod made the server dig or someone is hacking.
-    if (!digger.is_player()) return
-
-    const wieldedItem = digger.get_wielded_item()
-
-    // Something wild happened. If this is hit, there is an engine problem.
-    if (!wieldedItem) {
-      const name = digger.get_player_name()
-      minetest.log(LogLevel.warning, `Attempt to get wield item from player ${name} gave a null value!`)
-      return
-    }
-
-    const itemName = wieldedItem.get_name()
-
-    const def = minetest.registered_items[itemName]
-    // Something has gone seriously wrong, we'll role with it.
-    if (!def) {
-      // But we're going going to notify.
-      minetest.log(LogLevel.warning, `Tried to get an UNKNOWN item! ${itemName} does not exist!`)
-      return
-    }
-
-    // Make sure this is an actual tool.
-    const itemType = def.type
-
-    print(`type of ${itemName} is ${itemType}`)
-
-
-    const groups = def.groups
-    if (!groups) {
-      // Again, this is an error. If no groups are defined there's a problem with the item definition.
-     minetest.log(LogLevel.warning, `Item ${itemName} has no defined groups!`) 
-    }
-
-    // print(dump(def))
-    // print(dump(wieldedItem))
-  }
-
   minetest.handle_node_drops = function(position: Vec3, drops: string[], digger: ObjectRef) {
-
-    const groups = extractNodeGroups(position)
-
-    extractWieldGroups(digger)
-
-    // Group extraction has failed, abort.
-    if (!groups) return
-    
-    
-  
-
     for (const drop of drops) {
       const item = minetest.add_item(position, drop)
       if (!item) {
--- mods/testing/init.ts
+++ mods/testing/init.ts
@@ -2,4 +2,20 @@ namespace Testing {
   // minetest.register_globalstep((delta: number) => {
   //   print(`globalstep is: ${delta}`)
   // })
+  minetest.register_tool(":pickaxe", {
+    inventory_image: "default_tool_stonepick.png",
+    tool_capabilities: {
+      full_punch_interval: 0.5,
+      max_drop_level: 1,
+      groupcaps: {
+        stone: {
+          times: {
+            1: 1.0
+          },
+          maxlevel: 1,
+          maxdrop: 0
+        }
+      }
+    }
+  })
 }
