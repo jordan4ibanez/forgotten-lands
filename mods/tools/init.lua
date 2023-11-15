dd17a60124350c44b654edeb7ba168f3c75d287f
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2023-11-14 21:44:07 -0500

    Now delete it

--- mods/tools/init.lua
+++ mods/tools/init.lua
@@ -36,6 +36,7 @@ do
         end
         return temp
     end
-    Tools.generateDropRequirements({[Tools.ToolType.Axe] = 3})
+    local function generateToolLevels(____table)
+    end
     minetest.register_tool(":pickaxe", {inventory_image = "default_tool_stonepick.png", tool_capabilities = {full_punch_interval = 0.5, max_drop_level = 1, groupcaps = {stone = {times = {[1] = 1}, maxlevel = 1, maxdrop = 0}}}, groups = {pickaxe_1 = 1}})
 end
