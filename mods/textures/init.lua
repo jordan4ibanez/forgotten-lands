9fb3cfb074954680fd411e97e7dc727276e430b3
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2023-11-16 05:02:16 -0500

    Reorganize, add MIneclone2 shears

---
+++ mods/textures/init.lua
@@ -0,0 +1,12 @@
+do
+    local println = utility.println
+    println(
+        "-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-\n",
+        "| Pixel Perfection loaded.        |\n",
+        "| Creator: Sofar                  |\n",
+        "| Maintainers: Sofar & TacoTexMex |\n",
+        "| Copyright: CC-BY-SA-4.0         |\n",
+        "-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-\n"
+    )
+    println("shears from mineclone! CC-BY-SA-3.0!")
+end
---
+++ mods/textures/init.ts
@@ -0,0 +1,15 @@
+{
+  const println = utility.println;
+  println(
+    "-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-\n",
+    "| Pixel Perfection loaded.        |\n",
+    "| Creator: Sofar                  |\n",
+    "| Maintainers: Sofar & TacoTexMex |\n",
+    "| Copyright: CC-BY-SA-4.0         |\n",
+    "-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-\n"
+  )
+  println("shears from mineclone! CC-BY-SA-3.0!")
+
+  //! fixme: make this way less cluttered and pick a less strange texture pack.
+  //! either that or blend them.
+}
