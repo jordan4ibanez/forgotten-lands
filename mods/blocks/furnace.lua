6476d414b8f9eb537768d392e9309c2cd8aeb926
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2023-11-17 04:57:42 -0500

    Add widemouth furnace

--- mods/blocks/furnace.lua
+++ mods/blocks/furnace.lua
@@ -1,11 +1,101 @@
 Blocks = Blocks or ({})
 do
-    minetest.register_node(":furnace", {drawtype = Drawtype.normal, paramtype2 = ParamType2.facedir, is_ground_content = false, tiles = {
-        "default_furnace_top.png",
-        "default_furnace_bottom.png",
-        "default_furnace_side.png",
-        "default_furnace_side.png",
-        "default_furnace_side.png",
-        "default_furnace_front.png"
-    }})
+    local textureSize = 16
+    local function pixel(inputPixel)
+        return inputPixel / textureSize - 0.5
+    end
+    local furnaceNodeBox = {
+        type = Nodeboxtype.fixed,
+        fixed = {
+            {
+                pixel(0),
+                pixel(0),
+                pixel(0),
+                pixel(2),
+                pixel(16),
+                pixel(16)
+            },
+            {
+                pixel(14),
+                pixel(0),
+                pixel(0),
+                pixel(16),
+                pixel(16),
+                pixel(16)
+            },
+            {
+                pixel(0),
+                pixel(13),
+                pixel(0),
+                pixel(16),
+                pixel(16),
+                pixel(16)
+            },
+            {
+                pixel(0),
+                pixel(6),
+                pixel(0),
+                pixel(16),
+                pixel(9),
+                pixel(16)
+            },
+            {
+                pixel(0),
+                pixel(0),
+                pixel(0),
+                pixel(16),
+                pixel(1),
+                pixel(16)
+            },
+            {
+                pixel(0),
+                pixel(0),
+                pixel(2),
+                pixel(16),
+                pixel(16),
+                pixel(16)
+            }
+        }
+    }
+    local furnaceSelectionBox = {
+        type = Nodeboxtype.fixed,
+        fixed = {{
+            pixel(0),
+            pixel(0),
+            pixel(0),
+            pixel(16),
+            pixel(16),
+            pixel(16)
+        }}
+    }
+    minetest.register_node(":furnace", {
+        drawtype = Drawtype.nodebox,
+        paramtype2 = ParamType2.facedir,
+        is_ground_content = false,
+        node_box = furnaceNodeBox,
+        selection_box = furnaceSelectionBox,
+        tiles = {
+            "default_furnace_top.png",
+            "default_furnace_bottom.png",
+            "default_furnace_side.png",
+            "default_furnace_side.png",
+            "default_furnace_side.png",
+            "default_furnace_front.png"
+        }
+    })
+    minetest.register_node(":furnace_active", {
+        drawtype = Drawtype.nodebox,
+        paramtype2 = ParamType2.facedir,
+        is_ground_content = false,
+        node_box = furnaceNodeBox,
+        selection_box = furnaceSelectionBox,
+        tiles = {
+            "default_furnace_top.png",
+            "default_furnace_bottom.png",
+            "default_furnace_side.png",
+            "default_furnace_side.png",
+            "default_furnace_side.png",
+            "default_furnace_front_active.png"
+        }
+    })
 end
--- mods/blocks/furnace.ts
+++ mods/blocks/furnace.ts
@@ -1,8 +1,55 @@
 namespace Blocks {
+
+  const textureSize = 16
+  //! This should probably be a utility function.
+  function pixel(inputPixel: number): number {
+    return (inputPixel / textureSize) - 0.5
+  }
+
+
+  const furnaceNodeBox: NodeBox = {
+    type: Nodeboxtype.fixed,
+    fixed:[
+      [ // Left slice.
+        pixel(0),pixel(0),pixel(0),
+        pixel(2),pixel(16),pixel(16)
+      ],
+      [ // Right slice.
+        pixel(14),pixel(0),pixel(0),
+        pixel(16),pixel(16),pixel(16)
+      ],
+      [ // Top slice.
+        pixel(0),pixel(13),pixel(0),
+        pixel(16),pixel(16),pixel(16)
+      ],
+      [ // Center slice.
+        pixel(0),pixel(6),pixel(0),
+        pixel(16),pixel(9),pixel(16)
+      ],
+      [ // Bottom slice.
+        pixel(0),pixel(0),pixel(0),
+        pixel(16),pixel(1),pixel(16)
+      ],
+      [ // Inner core.
+        pixel(0),pixel(0),pixel(2),
+        pixel(16),pixel(16),pixel(16)
+      ],
+    ] 
+  }
+
+  const furnaceSelectionBox: NodeBox = {
+    type: Nodeboxtype.fixed,
+    fixed: [
+      [pixel(0),pixel(0),pixel(0),pixel(16),pixel(16),pixel(16)]
+    ]
+  }
+
   minetest.register_node(":furnace", {
-    drawtype: Drawtype.normal,
+    drawtype: Drawtype.nodebox,
     paramtype2: ParamType2.facedir,
     is_ground_content: false,
+    node_box: furnaceNodeBox,
+    selection_box: furnaceSelectionBox,
     tiles: [
       "default_furnace_top.png",
       "default_furnace_bottom.png",
@@ -10,6 +57,22 @@ namespace Blocks {
       "default_furnace_side.png",
 		  "default_furnace_side.png",
       "default_furnace_front.png"
+    ]
+  })
+
+  minetest.register_node(":furnace_active", {
+    drawtype: Drawtype.nodebox,
+    paramtype2: ParamType2.facedir,
+    is_ground_content: false,
+    node_box: furnaceNodeBox,
+    selection_box: furnaceSelectionBox,
+    tiles: [
+      "default_furnace_top.png",
+      "default_furnace_bottom.png",
+		  "default_furnace_side.png",
+      "default_furnace_side.png",
+		  "default_furnace_side.png",
+      "default_furnace_front_active.png"
     ]
   })
 }
