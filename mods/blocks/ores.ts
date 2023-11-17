05a004ad7aec85cd8410e439f88be4d1a353c946
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2023-11-17 03:07:45 -0500

    Add ores

--- mods/blocks/ores.ts
+++ mods/blocks/ores.ts
@@ -27,4 +27,80 @@ namespace Blocks {
     }
   })
 
+  minetest.register_node(":iron_ore", {
+    drawtype: Drawtype.normal,
+    tiles: oreIt("default_mineral_iron.png"),
+    sounds: sounds.stone(),
+    groups: {
+      [blockType.stone]: 2
+    },
+    drop: {
+      items: [
+        {
+          tool_groups: generateDropRequirements({
+            [ToolType.Pickaxe]: 2
+          }),
+          items: ["iron_ore"]
+        }
+      ]
+    }
+  })
+
+  minetest.register_node(":gold_ore", {
+    drawtype: Drawtype.normal,
+    tiles: oreIt("default_mineral_gold.png"),
+    sounds: sounds.stone(),
+    groups: {
+      [blockType.stone]: 2
+    },
+    drop: {
+      items: [
+        {
+          tool_groups: generateDropRequirements({
+            [ToolType.Pickaxe]: 2
+          }),
+          items: ["gold_ore"]
+        }
+      ]
+    }
+  })
+
+  minetest.register_node(":diamond_ore", {
+    drawtype: Drawtype.normal,
+    tiles: oreIt("default_mineral_diamond.png"),
+    sounds: sounds.stone(),
+    groups: {
+      [blockType.stone]: 3
+    },
+    drop: {
+      items: [
+        {
+          tool_groups: generateDropRequirements({
+            [ToolType.Pickaxe]: 3
+          }),
+          items: ["diamond"]
+        }
+      ]
+    }
+  })
+
+  minetest.register_node(":mese_ore", {
+    drawtype: Drawtype.normal,
+    tiles: oreIt("default_mineral_mese.png"),
+    sounds: sounds.stone(),
+    groups: {
+      [blockType.stone]: 4
+    },
+    drop: {
+      items: [
+        {
+          tool_groups: generateDropRequirements({
+            [ToolType.Pickaxe]: 4
+          }),
+          items: ["mese"]
+        }
+      ]
+    }
+  })
+
 }
