d92da02a829fcdfbbd90c39ce7564de742bbeea9
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2023-11-17 06:00:02 -0500

    Actually make this a utility library
    Probably going to get pretty complicated

---
+++ mods/formspec/init.ts
@@ -0,0 +1,9 @@
+namespace FormSpec {
+  // Extremely experimental formspec composable generation.
+  // Does a few things:
+  // 1.) Organizes better.
+  // 2.) Reads better.
+  // 3.) Enforces version 7.
+
+  
+}
--- mods/utility/formspec.lua
+++
@@ -1,1 +0,0 @@
-Utility = Utility or ({})
