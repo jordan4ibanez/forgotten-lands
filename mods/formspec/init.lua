78a49fae616cfe627b370ff5f7bc21045622c6d3
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2023-11-18 02:05:20 -0500

    Add more advanced interface

--- mods/formspec/init.lua
+++ mods/formspec/init.lua
@@ -1,20 +1,126 @@
+-- Lua Library inline imports
+local function __TS__Class(self)
+    local c = {prototype = {}}
+    c.prototype.__index = c.prototype
+    c.prototype.constructor = c
+    return c
+end
+
+local __TS__Symbol, Symbol
+do
+    local symbolMetatable = {__tostring = function(self)
+        return ("Symbol(" .. (self.description or "")) .. ")"
+    end}
+    function __TS__Symbol(description)
+        return setmetatable({description = description}, symbolMetatable)
+    end
+    Symbol = {
+        asyncDispose = __TS__Symbol("Symbol.asyncDispose"),
+        dispose = __TS__Symbol("Symbol.dispose"),
+        iterator = __TS__Symbol("Symbol.iterator"),
+        hasInstance = __TS__Symbol("Symbol.hasInstance"),
+        species = __TS__Symbol("Symbol.species"),
+        toStringTag = __TS__Symbol("Symbol.toStringTag")
+    }
+end
+
+local function __TS__InstanceOf(obj, classTbl)
+    if type(classTbl) ~= "table" then
+        error("Right-hand side of 'instanceof' is not an object", 0)
+    end
+    if classTbl[Symbol.hasInstance] ~= nil then
+        return not not classTbl[Symbol.hasInstance](classTbl, obj)
+    end
+    if type(obj) == "table" then
+        local luaClass = obj.constructor
+        while luaClass ~= nil do
+            if luaClass == classTbl then
+                return true
+            end
+            luaClass = luaClass.____super
+        end
+    end
+    return false
+end
+
+local function __TS__New(target, ...)
+    local instance = setmetatable({}, target.prototype)
+    instance:____constructor(...)
+    return instance
+end
+-- End of Lua Library inline imports
 formSpec = formSpec or ({})
 do
     local create = vector.create2d
+    formSpec.FormSpec = __TS__Class()
+    local FormSpec = formSpec.FormSpec
+    FormSpec.name = "FormSpec"
+    function FormSpec.prototype.____constructor(self, definition)
+        self.elements = {}
+        self.size = definition.size
+        self.fixedSize = definition.fixedSize
+        self.position = definition.position
+        self.anchor = definition.anchor
+        self.padding = definition.padding
+        self.disablePrepend = definition.disablePrepend
+        self.elements = definition.elements
+    end
+    function FormSpec.prototype.attachElement(self, newElement)
+        local ____self_elements_0 = self.elements
+        ____self_elements_0[#____self_elements_0 + 1] = newElement
+    end
+    formSpec.FormSpecContainer = __TS__Class()
+    local FormSpecContainer = formSpec.FormSpecContainer
+    FormSpecContainer.name = "FormSpecContainer"
+    function FormSpecContainer.prototype.____constructor(self)
+        self.position = create(0, 0)
+        self.elements = {}
+    end
+    function formSpec.newContainer(definition)
+        local temp = {position = definition.position, elements = definition.elements}
+        return temp
+    end
+    local function processElements(elementArray)
+        print(dump(elementArray))
+        for ____, element in ipairs(elementArray) do
+            print(__TS__InstanceOf(element, formSpec.FormSpecContainer))
+        end
+    end
     local function generate(d)
         local accumulator = "formspec_version[7]\n"
         if d.size then
-            local ____temp_0
+            local ____temp_1
             if d.fixedSize then
-                ____temp_0 = true
+                ____temp_1 = true
             else
-                ____temp_0 = false
+                ____temp_1 = false
             end
-            local fixed = ____temp_0
+            local fixed = ____temp_1
             local size = d.size
             accumulator = accumulator .. ((((("size[" .. tostring(size.x)) .. ",") .. tostring(size.y)) .. ",") .. tostring(fixed)) .. "]\n"
         end
-        print(accumulator)
+        if d.position then
+            local pos = d.position
+            accumulator = accumulator .. ((("position[" .. tostring(pos.x)) .. ",") .. tostring(pos.y)) .. "]\n"
+        end
+        if d.anchor then
+            local anchor = d.anchor
+            accumulator = accumulator .. ((("anchor[" .. tostring(anchor.x)) .. ",") .. tostring(anchor.y)) .. "]\n"
+        end
+        if d.padding then
+            local p = d.padding
+            accumulator = accumulator .. ((("padding[" .. tostring(p.x)) .. ",") .. tostring(p.y)) .. "]\n"
+        end
+        if d.disablePrepend then
+            accumulator = accumulator .. "no_prepend[]\n"
+        end
+        processElements(d.elements)
     end
-    generate({size = create(8, 9)})
+    generate(__TS__New(
+        formSpec.FormSpec,
+        {
+            size = create(8, 9),
+            elements = {}
+        }
+    ))
 end
--- mods/formspec/init.ts
+++ mods/formspec/init.ts
@@ -1,15 +1,86 @@
 namespace formSpec {
   // Extremely experimental formspec composable generation.
+  //
   // Does a few things:
+  //
   // 1.) Organizes better.
   // 2.) Reads better.
   // 3.) Enforces version 7.
+  //
+  // It is written as so (minorly verbose) to allow:
+  // 1.) Elemental compilation (constructor pattern)
+  // 2.) Literal compilation. An assignment via nested elements.
 
   const create = vector.create2d;
+
+  //? Definition root.
+
+  export interface FormsSpecDefinition {
+    size?: Vec2
+    fixedSize?: boolean
+    position?: Vec2
+    anchor?: Vec2
+    padding?: Vec2
+    disablePrepend?: boolean
+    elements: FormSpecElement[]
+  }
+
+  export class FormSpec {
+    size?: Vec2
+    fixedSize?: boolean
+    position?: Vec2
+    anchor?: Vec2
+    padding?: Vec2
+    disablePrepend?: boolean
+    elements: FormSpecElement[] = []
+
+    constructor(definition: FormsSpecDefinition) {
+      this.size = definition.size
+      this.fixedSize = definition.fixedSize
+      this.position = definition.position
+      this.anchor = definition.anchor
+      this.padding = definition.padding
+      this.disablePrepend = definition.disablePrepend
+      this.elements = definition.elements
+    }
+
+    attachElement(newElement: FormSpecElement): void {
+      this.elements.push(newElement)
+    }
+  }
+
+  // export function newDefinition(definition: FormsSpecDefinition): FormSpec {
+  //   const temp: FormSpec = {
+  //   }
+  //   return temp
+  // }
+
+  //? Element prototype.
 
   interface FormSpecElement {
 
   }
+
+  //? Container
+
+  export class FormSpecContainer implements FormSpecElement {
+    position: Vec2 = create(0,0)
+    elements: FormSpecElement[] = []
+  }
+  export interface FormSpecContainerDefinition {
+    position: Vec2
+    elements: FormSpecElement[]
+  }
+  export function newContainer(definition: FormSpecContainerDefinition): FormSpecContainer {
+    const temp: FormSpecContainer = {
+      position: definition.position,
+      elements: definition.elements
+    }
+    return temp
+  }
+
+
+  //? List
 
   export interface FormSpecList extends FormSpecElement {
     location: string
@@ -19,17 +90,14 @@ namespace formSpec {
     startingIndex: number
   }
 
-  export interface FormSpecDefinition {
-    size?: Vec2
-    fixedSize?: boolean
-    position?: Vec2
-    anchor?: Vec2
-    padding?: Vec2
-    disablePrepend?: boolean
-    container?: Vec2
+  function processElements(elementArray: FormSpecElement[]) {
+    print(dump(elementArray))
+    for (const element of elementArray) {
+      print(element instanceof FormSpecContainer)
+    }
   }
 
-  function generate(d: FormSpecDefinition) {
+  function generate(d: FormSpec) {
     //? figure out if newlines are necessary.
     //* note: components of formspecs are context sensitive.
     //* so this turns into a bunch of if-then checks in order.
@@ -55,12 +123,17 @@ namespace formSpec {
     if (d.disablePrepend) {
       accumulator += "no_prepend[]\n"
     }
-    print(accumulator)
+    // Now recurse all elements in the array.
+    processElements(d.elements)
+    // print(accumulator)
   }
 
-  generate({
-    size: create(8,9)
-  })
+  generate(new FormSpec({
+    size: create(8,9),
+    elements: [
+      
+    ]
+  }))
 
   
 }
