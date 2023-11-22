f20a34dbb3962c777c0103cad6aebaeed07efb01
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2023-11-22 05:13:12 -0500

    Workbench and mineclone2 attributes

--- mods/blocks/workbench.lua
+++ mods/blocks/workbench.lua
@@ -1,13 +1,110 @@
+-- Lua Library inline imports
+local function __TS__New(target, ...)
+    local instance = setmetatable({}, target.prototype)
+    instance:____constructor(...)
+    return instance
+end
+-- End of Lua Library inline imports
 blocks = blocks or ({})
 do
+    local create = vector.create2d
     local blockType = types.BlockType
+    local generate = formSpec.generate
+    local FormSpec = formSpec.FormSpec
+    local BackGround = formSpec.Background
+    local BGColor = formSpec.BGColor
+    local List = formSpec.List
+    local ListColors = formSpec.ListColors
+    local ListRing = formSpec.ListRing
+    local color = utility.color
+    local colorScalar = utility.colorScalar
+    local colorRGB = utility.colorRGB
+    local playerInventorySize = player.MAIN_INVENTORY_SIZE
+    blocks.WORKBENCH_INVENTORY_SIZE = create(3, 3)
+    local workBenchInventory = generate(__TS__New(
+        FormSpec,
+        {
+            size = create(12, 12),
+            elements = {
+                __TS__New(
+                    BGColor,
+                    {
+                        bgColor = colorScalar(85),
+                        fullScreen = "both",
+                        fullScreenbgColor = colorScalar(0, 40)
+                    }
+                ),
+                __TS__New(
+                    ListColors,
+                    {
+                        slotBGHover = colorScalar(70),
+                        slotBGNormal = colorScalar(55),
+                        slotBorder = colorScalar(0),
+                        toolTipBGColor = colorRGB(123, 104, 238),
+                        toolTipFontColor = colorScalar(100)
+                    }
+                ),
+                __TS__New(
+                    List,
+                    {
+                        location = "context",
+                        listName = "craft",
+                        position = create(5.5, 1.75),
+                        size = blocks.WORKBENCH_INVENTORY_SIZE,
+                        startingIndex = 0
+                    }
+                ),
+                __TS__New(
+                    List,
+                    {
+                        location = "context",
+                        listName = "output",
+                        position = create(9, 2.375),
+                        size = create(1, 1),
+                        startingIndex = 0
+                    }
+                ),
+                __TS__New(
+                    List,
+                    {
+                        location = "current_player",
+                        listName = "main",
+                        position = create(0.5, 6.5),
+                        size = create(playerInventorySize.x, 1),
+                        startingIndex = 0
+                    }
+                ),
+                __TS__New(
+                    List,
+                    {
+                        location = "current_player",
+                        listName = "main",
+                        position = create(0.5, 8),
+                        size = create(playerInventorySize.x, playerInventorySize.y - 1),
+                        startingIndex = playerInventorySize.x
+                    }
+                ),
+                __TS__New(ListRing, {location = "current_player", listName = "main"}),
+                __TS__New(ListRing, {location = "current_player", listName = "craft"})
+            }
+        }
+    ))
     minetest.register_node(
         ":workbench",
         {
             drawtype = Drawtype.normal,
             tiles = {"default_wood.png"},
             sounds = sounds.wood(),
-            groups = {[blockType.wood] = 1}
+            groups = {[blockType.wood] = 1},
+            on_construct = function(position)
+                local meta = minetest.get_meta(position)
+                local inventory = meta:get_inventory()
+                meta:set_string("formspec", workBenchInventory)
+                inventory:set_size("craft", blocks.WORKBENCH_INVENTORY_SIZE.x * blocks.WORKBENCH_INVENTORY_SIZE.y)
+                inventory:set_width("craft", blocks.WORKBENCH_INVENTORY_SIZE.x)
+                inventory:set_size("output", 1)
+                inventory:set_width("output", 1)
+            end
         }
     )
 end
--- mods/blocks/workbench.ts
+++ mods/blocks/workbench.ts
@@ -1,6 +1,106 @@
 namespace blocks {
 
+  const create = vector.create2d
+
   const blockType = types.BlockType;
+  const generate = formSpec.generate
+  const FormSpec = formSpec.FormSpec
+  const BackGround = formSpec.Background
+  const BGColor = formSpec.BGColor
+  const List = formSpec.List
+  const ListColors = formSpec.ListColors
+  const ListRing = formSpec.ListRing
+
+  const color = utility.color
+  const colorScalar = utility.colorScalar
+  const colorRGB = utility.colorRGB
+
+  const playerInventorySize = player.MAIN_INVENTORY_SIZE
+
+  export const WORKBENCH_INVENTORY_SIZE = create(3,3)
+  
+
+  const workBenchInventory: string = generate(new FormSpec({
+    size: create(12,12),
+    elements: [
+      //! Nice background colors.
+      new BGColor({
+        bgColor: colorScalar(85),
+        fullScreen: "both",
+        fullScreenbgColor: colorScalar(0,40)
+      }),
+      //! Make these lists look nice as well.
+      new ListColors({
+        slotBGHover: colorScalar(70),
+        slotBGNormal: colorScalar(55),
+        slotBorder: colorScalar(0),
+        toolTipBGColor: colorRGB(123,104,238),
+        toolTipFontColor: colorScalar(100)
+      }),
+      //! Craft area.
+      new List({
+        location: "context",
+        listName: "craft",
+        position: create(
+          5.5,
+          1.75
+        ),
+        size: WORKBENCH_INVENTORY_SIZE,
+        startingIndex: 0
+      }),
+      //! Craft output
+      new List({
+        location: "context",
+        listName: "output",
+        position: create(
+          9,
+          2.375
+        ),
+        size: create(
+          1,
+          1
+        ),
+        startingIndex: 0
+      }),
+      //! Hot bar.
+      new List({
+        location: "current_player",
+        listName: "main",
+        position: create(
+          0.5,
+          6.5
+        ),
+        size: create(
+          playerInventorySize.x,
+          1
+        ),
+        startingIndex: 0
+      }),
+      //! Main inventory.
+      new List({
+        location: "current_player",
+        listName: "main",
+        position: create(
+          0.5,
+          8
+        ),
+        size: create(
+          playerInventorySize.x,
+          playerInventorySize.y - 1
+        ),
+        startingIndex: playerInventorySize.x
+      }),
+      //! List Rings.
+      new ListRing({
+        location: "current_player",
+        listName: "main"
+      }),
+      new ListRing({
+        location: "current_player",
+        listName: "craft"
+      })
+    ]
+  }))
 
   minetest.register_node(":workbench", {
     drawtype: Drawtype.normal,
@@ -10,6 +110,16 @@ namespace blocks {
     sounds: sounds.wood(),
     groups: {
       [blockType.wood]: 1
+    },
+
+    on_construct(position: Vec3) {
+      const meta = minetest.get_meta(position)
+      const inventory = meta.get_inventory()
+      meta.set_string("formspec", workBenchInventory)
+      inventory.set_size("craft", WORKBENCH_INVENTORY_SIZE.x * WORKBENCH_INVENTORY_SIZE.y)
+      inventory.set_width("craft", WORKBENCH_INVENTORY_SIZE.x)
+      inventory.set_size("output", 1)
+      inventory.set_width("output", 1)
     }
   })
 }
