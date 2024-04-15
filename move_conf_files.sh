95bc23129ed83d91182601be44af60c128af246d
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2024-04-15 04:51:52 -0400

    Create move_conf_files.sh

---
+++ move_conf_files.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+# I'm sure there's an easier way to do this.
+# Simply copy over the conf files into the adjacent compiled directories.
+for filename in source/*; do
+  mod_file_name=${filename//"source/"/};
+  old_dir="source/${mod_file_name}/mod.conf";
+  new_dir="mods/${mod_file_name}/mod.conf";
+  cp $old_dir $new_dir
+done
