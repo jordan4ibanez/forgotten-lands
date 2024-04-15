79f31a294db3ac8f4ee55aa4635072c4d54975dc
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2024-04-15 11:18:54 -0400

    This cow sure is noisey

--- source/mob/init.ts
+++ source/mob/init.ts
@@ -1,17 +1,26 @@
 namespace mob {
 
   const fakeRef = utility.fakeRef;
+  const loadFiles = utility.loadFiles;
 
   class MobBase implements LuaEntity {
     name: string = "mob_base"
     object: ObjectRef = fakeRef()
     hp = 10
+
+    on_step(delta: number, moveResult: MoveResult): void {
+      print("Not implemented");
+    }
   }
 
   // Oh I'm looking forward to this now.
   class Cow extends MobBase {
     name = "cow"
     hp = 5
+
+    on_step(delta: number, moveResult: MoveResult): void {
+      print("I'm a cow, moo");
+    }
   }
 
   minetest.registerTSEntity(Cow);
