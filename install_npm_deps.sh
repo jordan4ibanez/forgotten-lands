089e0ce0fd2a06d690947f8cbd7a40ab34dd1d27
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2024-04-15 05:03:33 -0400

    Fix this npm disaster

--- install_npm_deps.sh
+++ install_npm_deps.sh
@@ -1,3 +1,5 @@
-sudo npm install --save-dev typescript-to-lua typescript
-sudo npm install --save-dev lua-types
-sudo npm install --save-dev minetesthelper 
+npm install -D typescript-to-lua typescript lua-types minetesthelper
+
+# you can install everything globally if you want but you still need
+# the local (-D) packages for vscode to work.
+#sudo npm install -g typescript-to-lua typescript lua-types minetesthelper
