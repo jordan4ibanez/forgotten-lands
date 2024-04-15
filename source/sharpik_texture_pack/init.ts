3d8e065872ee44d0a7b57dd5ed38991534c264ad
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2024-04-15 05:03:48 -0400

    Make this thing able to be accepted into contentdb

---
+++ source/sharpik_texture_pack/init.ts
@@ -0,0 +1,5 @@
+namespace textures {
+  print("SharpNet Photo Realism 64px texture pack brought to you by: Spharpik");
+  print("Link: https://content.minetest.net/packages/Sharpik/sharpnet_textures/");
+  print("License: GPL-3.0-only");
+}
