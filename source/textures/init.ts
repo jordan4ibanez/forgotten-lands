a601d8ab84d076e636caca3958689e7de14ab2cc
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2024-04-15 02:24:04 -0400

    Goodbye textures

--- source/textures/init.ts
+++ source/textures/init.ts
@@ -1,15 +1,5 @@
 namespace textures {
   const println = utility.println;
-  println(
-    "-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-\n",
-    "| Pixel Perfection loaded.        |\n",
-    "| Creator: Sofar                  |\n",
-    "| Maintainers: Sofar & TacoTexMex |\n",
-    "| Copyright: CC-BY-SA-4.0         |\n",
-    "-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-\n"
-  )
-  println("shears from mineclone! CC-BY-SA-3.0!")
 
-  //! fixme: make this way less cluttered and pick a less strange texture pack.
-  //! either that or blend them.
+  // Blame contentdb for not allowing this
 }
