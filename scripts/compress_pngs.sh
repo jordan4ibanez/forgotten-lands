65cd292113e2feecf89f0e6c4a16de72166fab44
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2024-04-16 05:51:40 -0400

    Let's try again

--- scripts/compress_pngs.sh
+++ scripts/compress_pngs.sh
@@ -3,13 +3,13 @@
 # Game textures.
 echo "Optimizing game textures..."
 for i in `find source/ -name "*.png" -type f`; do
-  "optipng $i"
+  optipng $i
 done
 
 # Menu textures.
 echo "Optimizing menu textures..."
 for i in `find menu/ -name "*.png" -type f`; do
-    "optipng $i"
+    optipng $i
 done
 
 echo "Done."
