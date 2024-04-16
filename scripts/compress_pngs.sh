bb1f77ff537c43e9171c1d59c93bce3d4cdf2e5c
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2024-04-16 05:50:40 -0400

    Create compress_pngs.sh

---
+++ scripts/compress_pngs.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Game textures.
+echo "Optimizing game textures..."
+for i in `find source/ -name "*.png" -type f`; do
+  "optipng $i"
+done
+
+# Menu textures.
+echo "Optimizing menu textures..."
+for i in `find menu/ -name "*.png" -type f`; do
+    "optipng $i"
+done
+
+echo "Done."
