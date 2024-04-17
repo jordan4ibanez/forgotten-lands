37c90067b42582e0fbbfcb98997c4ccecf54bde9
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2024-04-17 05:05:06 -0400

    Some of this nonsense

--- source/hand/init.ts
+++ source/hand/init.ts
@@ -1,15 +1,15 @@
 namespace hand {
   utility.registerNode("hand", {
-    // This texture is horrifying and I love it.
     tiles: ["character.png"],
     visual_scale: 1,
     wield_scale: vector.create3d(1, 1, 1),
     paramtype: utility.ParamType1.light,
-    
+
 
   })
 
   minetest.register_on_joinplayer((player: ObjectRef, _: string) => {
-    print(`hello, ${player.get_player_name()}!`)
+    // print(`hello, ${player.get_player_name()}!`)
+
   });
 }
