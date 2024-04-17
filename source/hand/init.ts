0296eeea43b95bc290c9dea2bba3d196354500de
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2024-04-17 11:16:00 -0400

    I can't believe my rust works lmao

--- source/hand/init.ts
+++ source/hand/init.ts
@@ -4,13 +4,20 @@ namespace hand {
     visual_scale: 1,
     wield_scale: vector.create3d(1, 1, 1),
     paramtype: ParamType1.light,
-
-
-
+    drawtype: Drawtype.mesh,
+    mesh: "hand.b3d"
   })
 
   minetest.register_on_joinplayer((player: ObjectRef, _: string) => {
-    // print(`hello, ${player.get_player_name()}!`)
 
+    // If the player's inventory disappears, uh oh.
+    utility.optionWrap<InvRef>(player.get_inventory())
+      .someFunction((inv: InvRef) => {
+        inv.set_size("hand", 1);
+        inv.set_stack("hand", 1, "hand");
+      })
+      .noneFunction(() => {
+        utility.warning("Player inventory disappeared when creating the 3D hand!");
+      })
   });
 }
