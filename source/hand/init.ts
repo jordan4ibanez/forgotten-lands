1054b7197d2bfe1e1cd2acf1e99f5f34cd04b4d9
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2024-04-17 05:00:43 -0400

    I dono't even know how they got string interpolation working like this in lua

--- source/hand/init.ts
+++ source/hand/init.ts
@@ -3,11 +3,11 @@ namespace hand {
     // This texture is horrifying and I love it.
     tiles: ["character.png"],
     visual_scale: 1,
-    wield_scale: new Vec3(1, 1, 1),
+    wield_scale: vector.create3d(1, 1, 1),
 
   })
 
   minetest.register_on_joinplayer((player: ObjectRef, _: string) => {
-
+    print(`hello, ${player.get_player_name()}!`)
   });
 }
