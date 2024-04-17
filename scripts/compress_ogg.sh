a1c77d3c879c6f933b0659ef1757367a0f759e53
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2024-04-17 10:19:32 -0400

    Keep current work, no use for it though

---
+++ scripts/compress_ogg.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# `ffmpeg -i $i -c:a libvorbis -ac 1 -ar 48000 -b:a 32k -vbr on "$" -y`
+
+# Game sounds.
+# echo "Optimizing game sounds..."
+# for i in `find source/ -name "*.ogg" -type f`; do
+#   ogg_stripped=${i//".ogg"/}
+#   echo `ffmpeg -i ${ogg_stripped}.ogg -c:a libvorbis -ac 1 -ar 48000 -b:a 32k ${ogg_stripped}_oof.ogg -y`
+#   echo `mv ${ogg_stripped}_oof.ogg ${ogg_stripped}.ogg`
+# done
+
+# Menu sounds.
+# echo "Optimizing menu sounds..."
+# for i in `find menu/ -name "*.ogg" -type f`; do
+#   ogg_stripped=${i//".ogg"/}
+#   echo `ffmpeg -i ${ogg_stripped}.ogg -c:a libvorbis -ac 1 -ar 48000 -b:a 32k ${ogg_stripped}_oof.ogg -y`
+#   echo `mv ${ogg_stripped}_oof.ogg ${ogg_stripped}.ogg`
+# done
+
+echo "Done."
