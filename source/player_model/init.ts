c936f647fede74ac0fa90f8057c495548f7ccc5a
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2024-04-18 09:07:14 -0400

    Tada

--- source/player_model/init.ts
+++ source/player_model/init.ts
@@ -5,7 +5,10 @@ namespace playerModel {
 
   minetest.register_on_joinplayer((player: ObjectRef) => {
     player.set_properties({
-
+      mesh: "character.b3d",
+      textures: ["character.png"],
+      visual: EntityVisual.mesh,
+      visual_size: vector.create3d(1, 1, 1)
     });
 
   });
