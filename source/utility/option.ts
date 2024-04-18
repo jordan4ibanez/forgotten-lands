fb52680eb7b038a9d09c731f6100fde8facfda32
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2024-04-18 01:24:29 -0400

    Optimize singlethreaded

--- source/utility/option.ts
+++ source/utility/option.ts
@@ -1,9 +1,10 @@
 namespace utility {
 
   /**
-   * Mini null safety wrapper 
+   * Mini null safety wrapper.
+   * Since this is single threaded, it's gonna get real weird.
    */
-  export class Option<T> {
+  export class OptionSingleThreaded<T> {
     data: T | null = null
 
     is_some(): boolean {
@@ -22,31 +23,40 @@ namespace utility {
       }
     }
 
-    someFunction<T>(f: (input: T) => void): Option<T> {
+    someFunction<T>(f: (input: T) => void): OptionSingleThreaded<T> {
       if (this.data != null) {
         f(this.data as T);
       }
-      return this as unknown as Option<T>;
+      return this as unknown as OptionSingleThreaded<T>;
     }
 
-    noneFunction<T>(f: () => void): Option<T> {
+    noneFunction<T>(f: () => void): OptionSingleThreaded<T> {
       if (this.data == null) {
         f();
       }
-      return this as unknown as Option<T>;
+      return this as unknown as OptionSingleThreaded<T>;
     }
 
     constructor(input: T | null) {
       this.data = input;
     }
+
+    mutate<Y>(input: Y | null): OptionSingleThreaded<Y> {
+      (this.data as Y | null) = input;
+      return this as unknown as OptionSingleThreaded<Y>;
+    }
   }
+
+  // Brute force memory optimization in this single threaded environment.
+  let singleton = new OptionSingleThreaded<any>(null);
 
   /**
    * If you're not sure if it's going to be nothing, safely wrap it.
+   * ! Do not use this in a threaded environment or it WILL explode. :D
    * @param input Something...or maybe nothing?
-   * @returns Option<Type>
+   * @returns OptionSingleThreaded<Type>
    */
-  export function optionWrap<T>(input: T | null): Option<T> {
-    return new Option<T>(input);
+  export function optionWrap<T>(input: T | null): OptionSingleThreaded<T> {
+    return singleton.mutate(input);
   }
 }
