4b3db4021350c5bc5964e5d6f39b63e0b2946152
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2024-04-18 02:36:00 -0400

    ANSI terminal RGB colorize

--- source/utility/init.ts
+++ source/utility/init.ts
@@ -103,6 +103,21 @@ namespace utility {
     minetest.register_node(":" + nodeName, definition);
   }
 
+
+  /**
+   * Colorize a string to pretty print it to an ANSI terminal.
+   * If your terminal does not support ANSI colors: Oops.
+   * @param inputString The string you want to print.
+   * @param r Red channel color. (0 - 255)
+   * @param g Green channel color. (0 - 255)
+   * @param b Blue channel color. (0 - 255)
+   * @returns ANSI Colorized string.
+   */
+  export function terminalColorize(inputString: string, r: number, g: number, b: number): string {
+    // tostring in case anything stupid happens.
+    return "\x1b[38;2;" + tostring(r) + ";" + tostring(g) + ";" + tostring(b) + "m" + inputString + "\x1b[0m";
+  }
+
   /**
    * Print a warning message instead of an error.
    * @param message The warning message.
