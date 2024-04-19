73ff12d5239c00860970d5ba329684f2fb3d49f3
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2024-04-19 05:16:29 -0400

    Optimize

--- source/utility/quaternion.ts
+++ source/utility/quaternion.ts
@@ -1,5 +1,12 @@
 namespace utility {
-  const PI_HALF = math.pi / 2;
+
+  const sin = math.sin;
+  const cosFromSin = math.cosFromSin;
+  const fma = math.fma;
+  const abs = math.abs;
+  const atan2 = math.atan2;
+  const invsqrt = math.invsqrt;
+  const safeAsin = math.safeAsin;
 
   /**
    * The only reason this exists is to smoothly interpolate euler rotation.
@@ -21,12 +28,12 @@ namespace utility {
     }
 
     fromVec(vec: Vec3): void {
-      let sx = math.sin(vec.x * 0.5);
-      let cx = math.cosFromSin(sx, vec.x * 0.5);
-      let sy = math.sin(vec.y * 0.5);
-      let cy = math.cosFromSin(sy, vec.y * 0.5);
-      let sz = math.sin(vec.z * 0.5);
-      let cz = math.cosFromSin(sz, vec.z * 0.5);
+      let sx = sin(vec.x * 0.5);
+      let cx = cosFromSin(sx, vec.x * 0.5);
+      let sy = sin(vec.y * 0.5);
+      let cy = cosFromSin(sy, vec.y * 0.5);
+      let sz = sin(vec.z * 0.5);
+      let cz = cosFromSin(sz, vec.z * 0.5);
 
       let cycz = cy * cz;
       let sysz = sy * sz;
@@ -46,31 +53,31 @@ namespace utility {
     }
 
     slerp(target: Quaternion, alpha: number): void {
-      let cosom = math.fma(this.x, target.x, math.fma(this.y, target.y, math.fma(this.z, target.z, this.w * target.w)));
-      let absCosom = math.abs(cosom);
+      let cosom = fma(this.x, target.x, fma(this.y, target.y, fma(this.z, target.z, this.w * target.w)));
+      let absCosom = abs(cosom);
       let scale0, scale1;
       if (1.0 - absCosom > 1E-6) {
         let sinSqr = 1.0 - absCosom * absCosom;
-        let sinom = math.invsqrt(sinSqr);
-        let omega = math.atan2(sinSqr * sinom, absCosom);
-        scale0 = (math.sin((1.0 - alpha) * omega) * sinom);
-        scale1 = (math.sin(alpha * omega) * sinom);
+        let sinom = invsqrt(sinSqr);
+        let omega = atan2(sinSqr * sinom, absCosom);
+        scale0 = (sin((1.0 - alpha) * omega) * sinom);
+        scale1 = (sin(alpha * omega) * sinom);
       } else {
         scale0 = 1.0 - alpha;
         scale1 = alpha;
       }
       scale1 = cosom >= 0.0 ? scale1 : -scale1;
-      this.x = math.fma(scale0, this.x, scale1 * target.x);
-      this.y = math.fma(scale0, this.y, scale1 * target.y);
-      this.z = math.fma(scale0, this.z, scale1 * target.z);
-      this.w = math.fma(scale0, this.w, scale1 * target.w);
+      this.x = fma(scale0, this.x, scale1 * target.x);
+      this.y = fma(scale0, this.y, scale1 * target.y);
+      this.z = fma(scale0, this.z, scale1 * target.z);
+      this.w = fma(scale0, this.w, scale1 * target.w);
     }
 
     // Mutates in place to prevent excessive objects.
     toVec3(mutation: Vec3): void {
-      mutation.x = math.atan2(this.x * this.w - this.y * this.z, 0.5 - this.x * this.x - this.y * this.y);
-      mutation.y = math.safeAsin(2.0 * (this.x * this.z + this.y * this.w));
-      mutation.z = math.atan2(this.z * this.w - this.x * this.y, 0.5 - this.y * this.y - this.z * this.z);
+      mutation.x = atan2(this.x * this.w - this.y * this.z, 0.5 - this.x * this.x - this.y * this.y);
+      mutation.y = safeAsin(2.0 * (this.x * this.z + this.y * this.w));
+      mutation.z = atan2(this.z * this.w - this.x * this.y, 0.5 - this.y * this.y - this.z * this.z);
     }
 
   }
