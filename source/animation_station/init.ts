422f19e6abaf429bf50810bd7a6799f323e4c942
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2024-04-20 12:22:55 -0400

    Base procedural animations

--- source/animation_station/init.ts
+++ source/animation_station/init.ts
@@ -1,9 +1,34 @@
 namespace animationStation {
 
+  const warning = utility.warning;
 
+  /**
+   * This is the base of the animation for players.
+   * Players are a special case because they don't work like
+   * lua entities. They are pure userdata. It's EXTREMELY ANNOYING.
+   */
   class PlayerAnimationState {
     animationProgress: number = 0;
   }
+
+  /**
+   * AnimationPoint defines TRS for a frame of the animation.
+   */
+  interface AnimationPoint {
+    translation: Vec3,
+    rotation: Vec3,
+    scale: Vec3,
+  }
+
+  interface AnimationRegister {
+    //? If I ever feel like making longer animations, I can use this.
+    // frames: { [key: number]: AnimationPoint; } = {};
+
+    start: AnimationPoint;
+    end: AnimationPoint;
+  }
+
+  let animationRepository:  = {};
 
   let playerAnimationState: { [key: string]: PlayerAnimationState; } = {};
 
@@ -20,6 +45,7 @@ namespace animationStation {
     playerAnimationState[name] = new PlayerAnimationState();
     return 0;
   };
+
 
   export function setPlayerAnimationProgress(player: ObjectRef, newValue: number): void {
     let name = player.get_player_name();
