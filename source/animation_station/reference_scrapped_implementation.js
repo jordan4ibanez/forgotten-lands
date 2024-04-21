c7cf53b81253e53aa70371d140e69a43c924d0b8
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2024-04-21 11:23:58 -0400

    Add getting start and end

--- source/animation_station/reference_scrapped_implementation.js
+++ source/animation_station/reference_scrapped_implementation.js
@@ -26,44 +26,7 @@ namespace animationStation {
     scale: Vec3;
   }
 
-  /**
-   * Internal worker for Animation Point.
-   */
-  class AnimationWorker implements AnimationPoint {
-    translation: Vec3 = vector.create3d(0, 0, 0);
-    rotation: Vec3 = vector.create3d(0, 0, 0);
-    scale: Vec3 = vector.create3d(1, 1, 1);
 
-    /**
-     * You can see this is so verbose specifically not to create new objects.
-     */
-    identity(): void {
-      this.translation.x = 0;
-      this.translation.y = 0;
-      this.translation.z = 0;
-      this.rotation.x = 0;
-      this.rotation.y = 0;
-      this.rotation.z = 0;
-      this.scale.x = 1;
-      this.scale.y = 1;
-      this.scale.z = 1;
-    }
-
-    /**
-     * So is this one.
-     */
-    set(trsObject: AnimationPoint): void {
-      this.translation.x = trsObject.translation.x;
-      this.translation.y = trsObject.translation.y;
-      this.translation.z = trsObject.translation.z;
-      this.rotation.x = trsObject.rotation.x;
-      this.rotation.y = trsObject.rotation.y;
-      this.rotation.z = trsObject.rotation.z;
-      this.scale.x = trsObject.scale.x;
-      this.scale.y = trsObject.scale.y;
-      this.scale.z = trsObject.scale.z;
-    }
-  }
 
   /**
    * Animation Register defines a basic animation for a bone.
@@ -107,71 +70,7 @@ namespace animationStation {
   let rotationEnd = new Quaternion(vector.create3d(0, math.pi, 0));
   let workerRotation = new Quaternion(vector.create3d(0, 0, 0));
   let workerVec = vector.create3d(0, 0, 0);
-  /**
-   * And the final processor, this guy.
-   */
-  class BoneOverrideWorker {
-    override: BoneOverride = {
-      position: {
-        vec: create3d(0, 0, 0),
-        interpolation: 0,
-        absolute: false
-      },
-      rotation: {
-        vec: create3d(0, 0, 0),
-        interpolation: 0,
-        absolute: false
-      },
-      scale: {
-        vec: create3d(1, 1, 1),
-        interpolation: 0,
-        absolute: true
-      }
-    };
 
-    identity(): void {
-      if (this.override.position != null) {
-        this.override.position.vec.x = 0;
-        this.override.position.vec.y = 0;
-        this.override.position.vec.z = 0;
-      }
-      if (this.override.rotation != null) {
-        this.override.rotation.vec.x = 0;
-        this.override.rotation.vec.y = 0;
-        this.override.rotation.vec.z = 0;
-      }
-      if (this.override.scale != null) {
-        this.override.scale.vec.x = 1;
-        this.override.scale.vec.y = 1;
-        this.override.scale.vec.z = 1;
-      }
-    }
-
-    setPosition(vec: Vec3): void {
-      if (this.override.position == null) {
-        error("Position went null in bone override worker.");
-      }
-      this.override.position.vec.x = vec.x;
-      this.override.position.vec.y = vec.y;
-      this.override.position.vec.z = vec.z;
-    }
-    setRotation(vec: Vec3): void {
-      if (this.override.rotation == null) {
-        error("Rotation went null in bone override worker.");
-      }
-      this.override.rotation.vec.x = vec.x;
-      this.override.rotation.vec.y = vec.y;
-      this.override.rotation.vec.z = vec.z;
-    }
-    setScale(vec: Vec3): void {
-      if (this.override.scale == null) {
-        error("Scale went null in bone override worker.");
-      }
-      this.override.scale.vec.x = vec.x;
-      this.override.scale.vec.y = vec.y;
-      this.override.scale.vec.z = vec.z;
-    }
-  }
 
   let boneOverrideWorker: BoneOverrideWorker = new BoneOverrideWorker();
 
