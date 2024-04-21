756b8cf15eb0c4b8450c891485221941de82a8bd
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2024-04-21 08:53:11 -0400

    Now neaten up the implementation

--- source/animation_station/init.ts
+++ source/animation_station/init.ts
@@ -3,54 +3,12 @@ namespace animationStation {
   const warning = utility.warning;
   const Quaternion = utility.Quaternion;
   const create3d = vector.create3d;
+  const loadFiles = utility.loadFiles;
 
-  //? As you can see, this is the nesting doll of protection.
+  loadFiles(["container"]);
 
-  /**
-   * Keyframe for an Animation.
-   */
-  export interface Keyframe {
-    translation: Vec3;
-    rotation: Vec3;
-    scale: Vec3;
-  }
+  let repository = new ModelContainer();
 
-  /**
-   * Animation holds 2 keyframes. (start and end)
-   */
-  export interface Animation {
-    start: Keyframe;
-    end: Keyframe;
-  }
+  
 
-  /**
-   * Animation Container contains a map of Animations.
-   */
-  export interface AnimationContainer {
-    /**
-     * String is the animation name.
-     */
-    animations: Map<string, Animation>;
-  }
-
-  /**
-   * Contains Bones.
-   *! (Please do not make a boneless version of this)
-   */
-  export interface BoneContainer {
-    /**
-     * String is the bone name.
-     */
-    bones: Map<string, AnimationContainer>;
-  }
-
-  /**
-   * Holds the Models..
-   */
-  export interface ModelContainer {
-    /**
-     * String is the model name.
-     */
-    models: Map<string, BoneContainer>;
-  }
 }
