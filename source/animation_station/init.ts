1f75c962f84c7e5c93e93e8e864d538a3b2e9f29
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2024-04-21 08:34:44 -0400

    Version 2

--- source/animation_station/init.ts
+++ source/animation_station/init.ts
@@ -4,5 +4,53 @@ namespace animationStation {
   const Quaternion = utility.Quaternion;
   const create3d = vector.create3d;
 
+  //? As you can see, this is the nesting doll of protection.
 
+  /**
+   * Keyframe for an Animation.
+   */
+  export interface Keyframe {
+    translation: Vec3;
+    rotation: Vec3;
+    scale: Vec3;
+  }
+
+  /**
+   * Animation holds 2 keyframes. (start and end)
+   */
+  export interface Animation {
+    start: Keyframe;
+    end: Keyframe;
+  }
+
+  /**
+   * Animation Container contains a map of Animations.
+   */
+  export interface AnimationContainer {
+    /**
+     * String is the animation name.
+     */
+    animations: Map<string, Animation>;
+  }
+
+  /**
+   * Contains Bones.
+   *! (Please do not make a boneless version of this)
+   */
+  export interface BoneContainer {
+    /**
+     * String is the bone name.
+     */
+    bones: Map<string, AnimationContainer>;
+  }
+
+  /**
+   * Holds the Models..
+   */
+  export interface ModelContainer {
+    /**
+     * String is the model name.
+     */
+    models: Map<string, BoneContainer>;
+  }
 }
