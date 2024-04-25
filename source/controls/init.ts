1e5db8bf37796348bce9a0844733932616431f90
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2024-04-25 05:33:15 -0400

    Some basic functionality

--- source/controls/init.ts
+++ source/controls/init.ts
@@ -9,10 +9,50 @@ namespace controls {
    * 
    */
 
+  class PlayerControls implements Keys {
+    up: boolean = false;
+    down: boolean = false;
+    left: boolean = false;
+    right: boolean = false;
+    jump: boolean = false;
+    aux1: boolean = false;
+    sneak: boolean = false;
+    dig: boolean = false;
+    place: boolean = false;
+    LMB: boolean = false;
+    RMB: boolean = false;
+    zoom: boolean = false;
+  }
 
+  let repository: Map<string, PlayerControls> = new Map();
+
+
+  // Add player to the repository when they join.
   minetest.register_on_joinplayer((player: ObjectRef) => {
-    const gotten_control_module: Keys = player.get_player_control();
+    const name = player.get_player_name();
+
+    repository.set(name, new PlayerControls);
+
+    // const playerControl: Keys = player.get_player_control();
   });
+  // Remove player from the repository when they leave.
+  minetest.register_on_leaveplayer((player: ObjectRef) => {
+    const name = player.get_player_name();
+    repository.delete(name);
+  });
+
+  function pollPlayerControls(player: ObjectRef): void {
+
+  }
+
+  // Poll each players inputs on every server step.
+  minetest.register_globalstep(() => {
+    for (let player of minetest.get_connected_players()) {
+      pollPlayerControls(player);
+    }
+  });
+
+
 
 
 
