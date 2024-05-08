cc916820c9ce3547fc2e8c38b5a9f501ec9d31c2
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2024-05-08 04:46:38 -0400

    Update install_npm_deps.sh

--- install_npm_deps.sh
+++ install_npm_deps.sh
@@ -1,5 +1,5 @@
-npm install -D typescript-to-lua typescript lua-types minetesthelper
+# First global update/install
+sudo npm install -g typescript typescript-to-lua lua-types minetesthelper
 
-# you can install everything globally if you want but you still need
-# the local (-D) packages for vscode to work.
-#sudo npm install -g typescript-to-lua typescript lua-types minetesthelper
+# Then local.
+npm install -D typescript typescript-to-lua lua-types minetesthelper
