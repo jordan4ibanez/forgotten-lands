250c197e4ccacd64d2e06991ff161571f8e356dc
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2024-05-09 02:37:44 -0400

    Unexplode the script

--- scripts/copy_textures.sh
+++ scripts/copy_textures.sh
@@ -2,9 +2,9 @@
 # This hackjob copies over all the textures into the compiled build.
 # EVERY. TIME.
 echo "Trying to make the textures directory..."
-mkdir mods/sharpik_texture_pack/textures/
+mkdir mods/textures/textures/
 echo "Done."
 
 echo "Copying over texture files..."
-cp -r source/sharpik_texture_pack/textures/* mods/sharpik_texture_pack/textures/
+cp -r source/textures/textures/* mods/textures/textures/
 echo "Done."
