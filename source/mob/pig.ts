08747b67ce88e59fdbd961fab2c6d33bf5041d1a
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2024-05-09 02:38:51 -0400

    A pig, truly incredible

--- source/mob/pig.ts
+++ source/mob/pig.ts
@@ -1,14 +1,14 @@
 namespace mob {
 
   // Oh I'm looking forward to this now.
-  class Cow extends Mob {
-    name = "cow";
+  class Pig extends Mob {
+    name = "pig";
     hp = 5;
 
     on_step(delta: number, moveResult: MoveResult): void {
-      // print("I'm a cow, moo");
+      print("Oink");
     }
   }
 
-  minetest.registerTSEntity(Cow);
+  minetest.registerTSEntity(Pig);
 }
