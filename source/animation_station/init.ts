4022cdeebde8b404dba4e5364bdaa5bc31e059a5
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2024-05-09 02:43:46 -0400

    Fix autocomplete

--- source/animation_station/init.ts
+++ source/animation_station/init.ts
@@ -244,6 +244,19 @@ namespace animationStation {
   export class AnimatedEntity implements LuaEntity {
     name: string = "Nothing";
     object: ObjectRef = fakeRef();
+
+    // Abstract members.
+    initial_properties?: ObjectProperties | undefined;
+    on_activate?(staticData: string, delta: number): void;
+    on_deactivate?(removal: boolean): void;
+    on_step?(delta: number, moveResult: MoveResult): void;
+    on_punch?(puncher: ObjectRef, timeFromLastPunch: number, toolCapabilities: ToolCapabilities, dir: Vec3, damage: number): void;
+    on_death?(killer: ObjectRef): void;
+    on_rightclick?(clicker: ObjectRef): void;
+    on_attach_child?(child: ObjectRef): void;
+    on_detach_child?(child: ObjectRef): void;
+    on_detach?(parent: ObjectRef): void;
+    get_staticdata?(): string;
   }
 
 }
