f875807c36c392ce6abfdd1d7ae1f8f0ff0d08a2
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2024-05-25 15:55:47 -0400

    Begin fixing ancient bug

--- source/sounds/place_sounds.ts
+++ source/sounds/place_sounds.ts
@@ -1,3 +1,11 @@
 namespace sounds {
-  print("Heblo")
+
+  // Server runs the place sound so they actually play normally.
+  minetest.register_on_placenode(function (pos: Vec3, node: NodeTable) {
+    print(dump(pos));
+
+    const name = node.name;
+
+    print(name);
+  });
 }
