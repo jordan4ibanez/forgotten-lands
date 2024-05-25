2091403aa3f3c81b6d463f1b5952e954448eaef9
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2024-05-25 18:00:03 -0400

    Server side audio fix

--- source/sounds/place_sounds.ts
+++ source/sounds/place_sounds.ts
@@ -1,11 +1,43 @@
 namespace sounds {
 
+
+  const playSound = minetest.sound_play;
+
+  // Cache this pointer.
+  const nodes = minetest.registered_nodes;
+
   // Server runs the place sound so they actually play normally.
-  minetest.register_on_placenode(function (pos: Vec3, node: NodeTable) {
-    print(dump(pos));
+  //? This is a fix for an ancient bug in minetest's audio handling.
+  minetest.register_on_placenode(function (position: Vec3, node: NodeTable) {
 
     const name = node.name;
 
-    print(name);
+    const definition = nodes[name];
+
+    if (definition == null) {
+      error("Tried to get a null definition of node " + name + "! Bailing out.");
+    }
+
+    const sounds = definition.sounds;
+
+    if (sounds == null) {
+      return;
+    }
+
+    const placed = sounds.placed;
+
+    if (placed == null) {
+      return;
+    }
+
+    // print(`Playing sound ${placed.name} at ${position.x} | ${position.y} | ${position.z}`);
+    playSound({
+      name: placed.name,
+      gain: placed.gain,
+      pitch: placed.pitch,
+    }, {
+      pos: position,
+      max_hear_distance: 16
+    });
   });
 }
