cf9f72758d77805eaa3c65dd3d52e7184aaaa093
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2024-05-26 07:59:52 -0400

    Fire exists

--- source/fire/init.ts
+++ source/fire/init.ts
@@ -1,7 +1,24 @@
 namespace fire {
   const loadFiles = utility.loadFiles;
 
-  
+  utility.registerNode("fire", {
+    drawtype: Drawtype.firelike,
+    tiles: [{
+      name: "fire_basic_flame_animated.png",
+      animation: {
+        aspect_h: 16,
+        aspect_w: 16,
+        type: TileAnimationType.vertical_frames,
+        length: 1
+      }
+    }],
+    collision_box: {
+      type: Nodeboxtype.fixed,
+      fixed: [
+        [-0.5,]
+      ]
+    }
+  });
 
   loadFiles(["smoke"]);
 }
