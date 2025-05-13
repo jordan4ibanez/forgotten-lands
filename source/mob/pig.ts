f6d256d3fdae788accd0a63fd1b4db0668ef2958
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2025-05-13 04:09:25 -0400

    Update pig.ts

--- source/mob/pig.ts
+++ source/mob/pig.ts
@@ -1,23 +1,23 @@
 namespace mob {
 
-  const create3d = vector.create3d;
+    const create3d = vector.create3d;
 
-  // Oh I'm looking forward to this now.
-  class Pig extends Mob {
-    name = "pig";
-    hp = 5;
+    // Oh I'm looking forward to this now.
+    class Pig extends Mob {
+        name = "pig";
+        hp = 5;
 
-    initial_properties: ObjectProperties = {
-      visual: EntityVisual.mesh,
-      mesh: "mobs_mc_pig.b3d",
-      textures: ["blank.png", "mobs_mc_pig.png", "mobs_mc_pig.png"],
-      visual_size: create3d(3, 3, 3)
-    };
+        initial_properties: ObjectProperties = {
+            visual: EntityVisual.mesh,
+            mesh: "mobs_mc_pig.b3d",
+            textures: ["blank.png", "mobs_mc_pig.png", "mobs_mc_pig.png"],
+            visual_size: create3d(3, 3, 3)
+        };
 
-    on_step(delta: number, moveResult: MoveResult): void {
-      print("Oink");
+        on_step(delta: number, moveResult: MoveResult): void {
+            print("Oink");
+        }
     }
-  }
 
-  utility.registerTSEntity(Pig);
+    utility.registerTSEntity(Pig);
 }
