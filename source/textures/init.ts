983077034a112be0d9736cc9c8f5d79a15ea1daa
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2025-05-13 04:10:47 -0400

    Update init.ts

--- source/textures/init.ts
+++ source/textures/init.ts
@@ -1,7 +1,7 @@
 namespace textures {
-  // print("Texture pack: REFI Textures");
-  // print("16px - Attempts to recapture the aesthetic of Minecraft, while improving upon visibility.");
-  // print("Brought to you by: MysticTempest");
-  // print("Link: https://content.minetest.net/packages/MysticTempest/refi_textures/");
-  // print("License: CC-BY-SA-4.0");
+    // print("Texture pack: REFI Textures");
+    // print("16px - Attempts to recapture the aesthetic of Minecraft, while improving upon visibility.");
+    // print("Brought to you by: MysticTempest");
+    // print("Link: https://content.minetest.net/packages/MysticTempest/refi_textures/");
+    // print("License: CC-BY-SA-4.0");
 }
