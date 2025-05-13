14ab343860936fda67d4ce41d216b8ba0464f6c8
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2025-05-13 04:08:37 -0400

    Update init.ts

--- source/hand/init.ts
+++ source/hand/init.ts
@@ -1,32 +1,32 @@
 namespace hand {
 
-  const optionWrap = utility.optionWrap;
-  type Option<T> = utility.Option<T>;
-  let warning = utility.warning;
+    const optionWrap = utility.optionWrap;
+    type Option<T> = utility.Option<T>;
+    let warning = utility.warning;
 
 
-  utility.registerNode("hand", {
-    tiles: ["character.png"],
-    visual_scale: 1,
-    wield_scale: vector.create3d(1, 1, 1),
-    paramtype: ParamType1.light,
-    drawtype: Drawtype.mesh,
-    mesh: "hand.b3d"
-  });
+    utility.registerNode("hand", {
+        tiles: ["character.png"],
+        visual_scale: 1,
+        wield_scale: vector.create3d(1, 1, 1),
+        paramtype: ParamType1.light,
+        drawtype: Drawtype.mesh,
+        mesh: "hand.b3d"
+    });
 
-  minetest.register_on_joinplayer((player: ObjectRef, _: string) => {
-    // If the player's inventory disappears, uh oh.
-    let result: Option<InvRef> = optionWrap<InvRef>(player.get_inventory());
-    switch (result.is_some()) {
-      case true: {
-        const inv = result.unwrap();
-        inv.set_size("hand", 1);
-        inv.set_stack("hand", 1, "hand");
-        break;
-      }
-      case false: {
-        warning("Player inventory disappeared when creating the 3D hand!");
-      }
-    }
-  });
+    minetest.register_on_joinplayer((player: ObjectRef, _: string) => {
+        // If the player's inventory disappears, uh oh.
+        let result: Option<InvRef> = optionWrap<InvRef>(player.get_inventory());
+        switch (result.is_some()) {
+            case true: {
+                const inv = result.unwrap();
+                inv.set_size("hand", 1);
+                inv.set_stack("hand", 1, "hand");
+                break;
+            }
+            case false: {
+                warning("Player inventory disappeared when creating the 3D hand!");
+            }
+        }
+    });
 }
