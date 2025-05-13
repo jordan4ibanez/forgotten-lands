37fdc197e389d2a7d9efd061a83132cf33e41a7b
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2025-05-13 04:09:57 -0400

    Update fuel.ts

--- source/recipes/fuel.ts
+++ source/recipes/fuel.ts
@@ -1,37 +1,37 @@
 
 namespace recipes {
 
-  const blockType = types.CraftingBlockType;
+    const blockType = types.CraftingBlockType;
 
-  minetest.register_craft({
-    type: CraftRecipeType.fuel,
-    recipe: "coal",
-    burntime: 40,
-  });
+    minetest.register_craft({
+        type: CraftRecipeType.fuel,
+        recipe: "coal",
+        burntime: 40,
+    });
 
-  minetest.register_craft({
-    type: CraftRecipeType.fuel,
-    recipe: "coal_block",
-    burntime: 370,
-  });
+    minetest.register_craft({
+        type: CraftRecipeType.fuel,
+        recipe: "coal_block",
+        burntime: 370,
+    });
 
-  minetest.register_craft({
-    type: CraftRecipeType.fuel,
-    recipe: blockType.tree,
-    burntime: 40,
-  });
+    minetest.register_craft({
+        type: CraftRecipeType.fuel,
+        recipe: blockType.tree,
+        burntime: 40,
+    });
 
-  minetest.register_craft({
-    type: CraftRecipeType.fuel,
-    recipe: blockType.planks,
-    burntime: 7,
-  });
+    minetest.register_craft({
+        type: CraftRecipeType.fuel,
+        recipe: blockType.planks,
+        burntime: 7,
+    });
 
-  minetest.register_craft({
-    type: CraftRecipeType.fuel,
-    recipe: "stick",
-    burntime: 1,
-  });
+    minetest.register_craft({
+        type: CraftRecipeType.fuel,
+        recipe: "stick",
+        burntime: 1,
+    });
 
 
 
