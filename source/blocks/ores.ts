9f286882c8f06e36fdfd74d66b2fac54a322fcb4
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2025-05-13 04:07:57 -0400

    Update ores.ts

--- source/blocks/ores.ts
+++ source/blocks/ores.ts
@@ -1,106 +1,106 @@
 
 namespace blocks {
-  const generateDropRequirements = tools.generateDropRequirements;
-  const ToolType = types.ToolType;
-  const blockType = types.BlockType;
+    const generateDropRequirements = tools.generateDropRequirements;
+    const ToolType = types.ToolType;
+    const blockType = types.BlockType;
 
-  function oreIt(oreTexture: string): string[] {
-    return ["default_stone.png^" + oreTexture];
-  }
-
-  utility.registerNode("coal_ore", {
-    drawtype: Drawtype.normal,
-    tiles: oreIt("default_mineral_coal.png"),
-    sounds: sounds.stone(),
-    groups: {
-      [blockType.stone]: 1
-    },
-    drop: {
-      items: [
-        {
-          tool_groups: generateDropRequirements({
-            [ToolType.Pickaxe]: 1
-          }),
-          items: ["coal"]
-        }
-      ]
+    function oreIt(oreTexture: string): string[] {
+        return ["default_stone.png^" + oreTexture];
     }
-  });
 
-  utility.registerNode("iron_ore", {
-    drawtype: Drawtype.normal,
-    tiles: oreIt("default_mineral_iron.png"),
-    sounds: sounds.stone(),
-    groups: {
-      [blockType.stone]: 2
-    },
-    drop: {
-      items: [
-        {
-          tool_groups: generateDropRequirements({
-            [ToolType.Pickaxe]: 2
-          }),
-          items: ["iron_ore"]
+    utility.registerNode("coal_ore", {
+        drawtype: Drawtype.normal,
+        tiles: oreIt("default_mineral_coal.png"),
+        sounds: sounds.stone(),
+        groups: {
+            [blockType.stone]: 1
+        },
+        drop: {
+            items: [
+                {
+                    tool_groups: generateDropRequirements({
+                        [ToolType.Pickaxe]: 1
+                    }),
+                    items: ["coal"]
+                }
+            ]
         }
-      ]
-    }
-  });
+    });
 
-  utility.registerNode("gold_ore", {
-    drawtype: Drawtype.normal,
-    tiles: oreIt("default_mineral_gold.png"),
-    sounds: sounds.stone(),
-    groups: {
-      [blockType.stone]: 2
-    },
-    drop: {
-      items: [
-        {
-          tool_groups: generateDropRequirements({
-            [ToolType.Pickaxe]: 2
-          }),
-          items: ["gold_ore"]
+    utility.registerNode("iron_ore", {
+        drawtype: Drawtype.normal,
+        tiles: oreIt("default_mineral_iron.png"),
+        sounds: sounds.stone(),
+        groups: {
+            [blockType.stone]: 2
+        },
+        drop: {
+            items: [
+                {
+                    tool_groups: generateDropRequirements({
+                        [ToolType.Pickaxe]: 2
+                    }),
+                    items: ["iron_ore"]
+                }
+            ]
         }
-      ]
-    }
-  });
+    });
 
-  utility.registerNode("diamond_ore", {
-    drawtype: Drawtype.normal,
-    tiles: oreIt("default_mineral_diamond.png"),
-    sounds: sounds.stone(),
-    groups: {
-      [blockType.stone]: 3
-    },
-    drop: {
-      items: [
-        {
-          tool_groups: generateDropRequirements({
-            [ToolType.Pickaxe]: 3
-          }),
-          items: ["diamond"]
+    utility.registerNode("gold_ore", {
+        drawtype: Drawtype.normal,
+        tiles: oreIt("default_mineral_gold.png"),
+        sounds: sounds.stone(),
+        groups: {
+            [blockType.stone]: 2
+        },
+        drop: {
+            items: [
+                {
+                    tool_groups: generateDropRequirements({
+                        [ToolType.Pickaxe]: 2
+                    }),
+                    items: ["gold_ore"]
+                }
+            ]
         }
-      ]
-    }
-  });
+    });
 
-  utility.registerNode("mese_ore", {
-    drawtype: Drawtype.normal,
-    tiles: oreIt("default_mineral_mese.png"),
-    sounds: sounds.stone(),
-    groups: {
-      [blockType.stone]: 4
-    },
-    drop: {
-      items: [
-        {
-          tool_groups: generateDropRequirements({
-            [ToolType.Pickaxe]: 4
-          }),
-          items: ["mese"]
+    utility.registerNode("diamond_ore", {
+        drawtype: Drawtype.normal,
+        tiles: oreIt("default_mineral_diamond.png"),
+        sounds: sounds.stone(),
+        groups: {
+            [blockType.stone]: 3
+        },
+        drop: {
+            items: [
+                {
+                    tool_groups: generateDropRequirements({
+                        [ToolType.Pickaxe]: 3
+                    }),
+                    items: ["diamond"]
+                }
+            ]
         }
-      ]
-    }
-  });
+    });
+
+    utility.registerNode("mese_ore", {
+        drawtype: Drawtype.normal,
+        tiles: oreIt("default_mineral_mese.png"),
+        sounds: sounds.stone(),
+        groups: {
+            [blockType.stone]: 4
+        },
+        drop: {
+            items: [
+                {
+                    tool_groups: generateDropRequirements({
+                        [ToolType.Pickaxe]: 4
+                    }),
+                    items: ["mese"]
+                }
+            ]
+        }
+    });
 
 }
