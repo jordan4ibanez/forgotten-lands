9a4819b5640990b105511ef0faed6dd379003c56
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2025-05-21 14:18:40 -0400

    uh oh

--- source/anti_complain/init.ts
+++ source/anti_complain/init.ts
@@ -1,5 +1,5 @@
 // Stop minetest from COMPLAINING ABOUT EVERYTHING!
-minetest.global_exists = (name: string): boolean => {
+core.global_exists = (name: string): boolean => {
     return true;
 };
 // This probably won't blow everything up, maybe.
