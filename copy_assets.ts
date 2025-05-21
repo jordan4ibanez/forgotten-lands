45f2f727b6ccd9350422bab54fd81c4393330324
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2025-05-21 18:21:17 -0400

    Welp that's already helping

--- copy_assets.ts
+++ copy_assets.ts
@@ -3,5 +3,13 @@ import * as FS from "node:fs";
 //? Copy media assets into the build.
 ["models", "sounds", "schematics", "textures"].forEach((id: string) => {
     FS.cpSync(`source/${id}/${id}`, `mods/${id}/${id}`, { recursive: true });
-})
+});
 
+//? Copy the config files.
+FS.readdirSync("source/", { recursive: false }).forEach((item: string | Buffer, index: number) => {
+    if (item instanceof Buffer) return;
+    if (!FS.statSync(`source/${item}`).isDirectory()) return;
+    if (!FS.existsSync(`source/${item}/mod.conf`)) throw new Error(`mod ${item} is missing a mod.conf!`);
+    console.log(`dir: ${item}`);
+
+});
