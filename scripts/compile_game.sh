a960084de9d29b4b353470296d3231c395e9ec85
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2025-05-21 18:56:20 -0400

    These are all now useless

--- scripts/compile_game.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-# echo Transpiling Crafter into lua...
-npx tstl
-# echo Successfully built Crafter.
--- scripts/copy_sounds.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-# This is horrible and I love it.
-# echo "Trying to make the sounds directory..."
-mkdir mods/sounds/sounds/
-# echo "Done."
-
-# echo "Copying over the sound files..."
-cp -r source/sounds/sounds/* mods/sounds/sounds/
-# echo "Done."
