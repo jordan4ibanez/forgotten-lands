2d20e412a575de2e43f1eac35fb73158de26e383
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2025-05-20 21:04:19 -0400

    Well that's kind of important

--- minetest-api.d.ts
+++ minetest-api.d.ts
@@ -115,7 +115,7 @@ interface core {
     remove_node(position: Vec3): void;
     get_node(position: Vec3): NodeTable;
     get_node_or_nil(position: Vec3): NodeTable | null;
-    get_node_light(position: Vec3, timeOfDay: number): number;
+    get_node_light(position: Vec3, timeOfDay: number): number | null;
     get_natural_light(position: Vec3, timeOfDay: number): number;
     get_artificial_light(param1: number): number;
     place_node(position: Vec3, nodeTable: NodeTable): void;
