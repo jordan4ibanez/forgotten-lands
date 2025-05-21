6155042cb37b8649aef6b9293e085ddb6ad69d5c
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2025-05-20 20:46:01 -0400

    Another function

--- source/new_functions/init.ts
+++ source/new_functions/init.ts
@@ -116,18 +116,6 @@ namespace newFunctions {
         data.touch_hurt_ticker = tick;
     }
 
-
-    // local pos
-    // local hurt
-    // local name
-    // local damage_nodes
-    // local real_nodes
-    // local a_min
-    // local a_max
-    // local damage_amount
-    // local gotten_node
-    // local _
-
     const a_min: Vec3 = vector.create3d();
     const a_max: Vec3 = vector.create3d();
 
@@ -184,25 +172,26 @@ namespace newFunctions {
 
 
 
-    // -- handle damage when inside node
-    // -- this is lua collision detection
+    // Handle damage when inside node.
+    // This is lua collision detection.
+    // Damages players 4 times a second.
+    function handle_hurt_inside(player: ObjectRef, damage: number, dtime: number): void {
+        const name: string = player.get_player_name();
 
-    // -- damages players 4 times a second
-    // local name
-    // local temp_pool
-    // local tick
-    // local handle_hurt_inside = function(player,damage,dtime)
-    // 	name      = player:get_player_name()
-    // 	temp_pool = pool[name]
-    // 	tick      = temp_pool.hurt_inside_ticker
+        const data: PType | undefined = pool.get(name);
+        if (!data) {
+            return;
+        }
 
-    // 	tick = tick - dtime
-    // 	if tick <= 0 then
-    // 		player:set_hp(player:get_hp()-damage)
-    // 		tick = 0.25
-    // 	end
-    // 	temp_pool.hurt_inside_ticker = tick
-    // end
+        var tick: number = data.hurt_inside_ticker;
+        tick -= dtime;
+
+        if (tick <= 0) {
+            player.set_hp(player.get_hp() - damage);
+            tick = 0.25;
+        }
+        data.hurt_inside_ticker = tick;
+    }
 
     // local pos
     // local hurt
