608bbd0d0347b60f785484c2a36ae9a204b5c27d
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2025-05-21 18:28:48 -0400

    Hooray!

--- copy_assets.ts
+++ copy_assets.ts
@@ -7,9 +7,16 @@ import * as FS from "node:fs";
 
 //? Copy the config files.
 FS.readdirSync("source/", { recursive: false }).forEach((item: string | Buffer) => {
+    // Basic checks to make sure nothing explodes.
     if (item instanceof Buffer) return;
     if (!FS.statSync(`source/${item}`).isDirectory()) return;
     if (!FS.existsSync(`source/${item}/mod.conf`)) throw new Error(`mod ${item} is missing a mod.conf!`);
-    console.log(`dir: ${item}`);
-
+    // So now, if the folder doesn't exist it needs to be made.
+    if (!FS.existsSync(`mods/${item}`)) {
+        FS.mkdirSync(`mods/${item}`);
+    } else {
+        // If a file is accidentally put in the mods folder during testing, this needs manual intervention.
+        if (!FS.statSync(`source/${item}`).isDirectory()) throw new Error(`mod ${item} is a file in the mods folder!`);
+    }
+    FS.copyFileSync(`source/${item}/mod.conf`, `mods/${item}/mod.conf`,);
 });
