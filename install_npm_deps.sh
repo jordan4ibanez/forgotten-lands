4c813a7c85f728e3ca453e1bdd09f0f4837bb5ae
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2025-05-21 20:19:20 -0400

    Delete install_npm_deps.sh

--- install_npm_deps.sh
+++
@@ -1,5 +0,0 @@
-# First global update/install
-sudo npm install -g typescript typescript-to-lua lua-types minetesthelper
-
-# Then local.
-npm install -D typescript typescript-to-lua lua-types minetesthelper
