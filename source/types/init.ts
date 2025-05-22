0b0a3347b21f680c706173d780c5d07cfd7e068e
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2025-05-22 12:07:19 -0400

    I forgot to format everything

--- source/types/init.ts
+++ source/types/init.ts
@@ -1,55 +1,55 @@
 namespace types {
-    export enum ToolType {
-        Pickaxe = "pickaxe",
-        Shovel = "shovel",
-        Axe = "axe",
-        Hoe = "hoe",
-        Sword = "sword",
-        Shears = "shears"
-    }
+	export enum ToolType {
+		Pickaxe = "pickaxe",
+		Shovel = "shovel",
+		Axe = "axe",
+		Hoe = "hoe",
+		Sword = "sword",
+		Shears = "shears",
+	}
 
-    export enum BlockType {
-        break_instant = "break_instant",
-        soil = "soil",
-        wood = "wood",
-        leaf = "leaf",
-        stone = "stone",
-        metal = "metal",
-        glass = "glass",
-        wool = "wool",
-        planks = "planks",
-        tree = "tree",
-        sand = "sand",
-        lava_cooling = "lava_cooling",
-        liquid = "liquid",
-        water = "water",
-        attached_node = "attached_node"
-    }
+	export enum BlockType {
+		break_instant = "break_instant",
+		soil = "soil",
+		wood = "wood",
+		leaf = "leaf",
+		stone = "stone",
+		metal = "metal",
+		glass = "glass",
+		wool = "wool",
+		planks = "planks",
+		tree = "tree",
+		sand = "sand",
+		lava_cooling = "lava_cooling",
+		liquid = "liquid",
+		water = "water",
+		attached_node = "attached_node",
+	}
 
-    // This one is quite useful for crafting recipes.
-    export enum CraftingBlockType {
-        soil = "group:soil",
-        wood = "group:wood",
-        leaf = "group:leaf",
-        stone = "group:stone",
-        metal = "group:metal",
-        glass = "group:glass",
-        wool = "group:wool",
-        planks = "group:planks",
-        tree = "group:tree",
-        sand = "group:sand"
-    }
+	// This one is quite useful for crafting recipes.
+	export enum CraftingBlockType {
+		soil = "group:soil",
+		wood = "group:wood",
+		leaf = "group:leaf",
+		stone = "group:stone",
+		metal = "group:metal",
+		glass = "group:glass",
+		wool = "group:wool",
+		planks = "group:planks",
+		tree = "group:tree",
+		sand = "group:sand",
+	}
 
-    export enum DamageGroup {
-        flesh = "flesh",
-        metal = "metal",
-        bone = "bone"
-    }
+	export enum DamageGroup {
+		flesh = "flesh",
+		metal = "metal",
+		bone = "bone",
+	}
 
-    export enum CraftRecipeType {
-        shapeless = "shapeless",
-        toolrepair = "toolrepair",
-        cooking = "cooking",
-        fuel = "fuel"
-    }
+	export enum CraftRecipeType {
+		shapeless = "shapeless",
+		toolrepair = "toolrepair",
+		cooking = "cooking",
+		fuel = "fuel",
+	}
 }
