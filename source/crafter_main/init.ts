56e6ccc6d612e64413db9ef5cb36f6bc5a8753e8
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2025-05-21 20:39:14 -0400

    I forgot I did magic

--- source/crafter_main/init.ts
+++ source/crafter_main/init.ts
@@ -5,20 +5,20 @@ namespace main {
     }
 
     utility.loadFiles([
-        `${path}/sounds.lua`,
-        `${path}/nodes.lua`,
-        `${path}/grass_spread.lua`,
-        `${path}/saplings.lua`,
-        `${path}/ore.lua`,
-        `${path}/items.lua`,
-        `${path}/schematics.lua`,
-        `${path}/mapgen.lua`,
-        `${path}/tools.lua`,
-        `${path}/settings.lua`,
-        `${path}/craft_recipes.lua`,
-        `${path}/falling.lua`,
-        `${path}/bucket.lua`,
-        `${path}/lava_cooling.lua`,
-        `${path}/command_overrides.lua`
+        "sounds",
+        "nodes",
+        "grass_spread",
+        "saplings",
+        "ore",
+        "items",
+        "schematics",
+        "mapgen",
+        "tools",
+        "settings",
+        "craft_recipes",
+        "falling",
+        "bucket",
+        "lava_cooling",
+        "command_overrides"
     ]);
 }
