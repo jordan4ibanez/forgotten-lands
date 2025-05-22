e1e6e26187597cf65d082bf5efc7bcb74bfa5bd9
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2025-05-21 20:42:56 -0400

    Update items.ts

--- source/crafter_main/items.ts
+++ source/crafter_main/items.ts
@@ -1,75 +1,75 @@
 // Item definition.
 
-core.register_food("main:apple",{
-	description = "Apple",
-	texture = "apple.png",
-	satiation=1,
-	hunger=2,
-})
+core.register_food("main:apple", {
+    description: "Apple",
+    texture: "apple.png",
+    satiation: 1,
+    hunger: 2,
+});
 
-core.register_food("main:sugar",{
-	description = "Sugar",
-	texture = "sugar.png",
-	satiation=1,
-	hunger=1,
-})
+core.register_food("main:sugar", {
+    description: "Sugar",
+    texture: "sugar.png",
+    satiation: 1,
+    hunger: 1,
+});
 
 core.register_craftitem("main:stick", {
-	description = "Stick",
-	inventory_image = "stick.png",
-	groups = {stick = 1}
-})
+    description: "Stick",
+    inventory_image: "stick.png",
+    groups: { stick: 1 }
+});
 core.register_craftitem("main:paper", {
-	description = "Paper",
-	inventory_image = "paper.png",
-	groups = {paper = 1}
-})
+    description: "Paper",
+    inventory_image: "paper.png",
+    groups: { paper: 1 }
+});
 
 core.register_craftitem("main:coal", {
-	description = "Coal",
-	inventory_image = "coal.png",
-	groups = {coal = 1}
-})
+    description: "Coal",
+    inventory_image: "coal.png",
+    groups: { coal: 1 }
+});
 
 core.register_craftitem("main:charcoal", {
-	description = "Charcoal",
-	inventory_image = "charcoal.png",
-	groups = {coal = 1}
-})
+    description: "Charcoal",
+    inventory_image: "charcoal.png",
+    groups: { coal: 1 }
+});
 
 core.register_craftitem("main:iron", {
-	description = "Iron",
-	inventory_image = "iron.png",
-})
+    description: "Iron",
+    inventory_image: "iron.png",
+});
 
 core.register_craftitem("main:gold", {
-	description = "Gold",
-	inventory_image = "gold.png",
-})
+    description: "Gold",
+    inventory_image: "gold.png",
+});
 
 core.register_craftitem("main:diamond", {
-	description = "Diamond",
-	inventory_image = "diamond.png",
-})
+    description: "Diamond",
+    inventory_image: "diamond.png",
+});
 core.register_craftitem("main:flint", {
-	description = "Flint",
-	inventory_image = "flint.png",
-})
+    description: "Flint",
+    inventory_image: "flint.png",
+});
 
 core.register_craftitem("main:lapis", {
-	description = "Lapis Lazuli",
-	inventory_image = "lapis.png",
-})
+    description: "Lapis Lazuli",
+    inventory_image: "lapis.png",
+});
 
 core.register_craftitem("main:emerald", {
-	description = "Emerald",
-	inventory_image = "emerald.png",
-})
+    description: "Emerald",
+    inventory_image: "emerald.png",
+});
 core.register_craftitem("main:sapphire", {
-	description = "Sapphire",
-	inventory_image = "sapphire.png",
-})
+    description: "Sapphire",
+    inventory_image: "sapphire.png",
+});
 core.register_craftitem("main:ruby", {
-	description = "Ruby",
-	inventory_image = "ruby.png",
-})
+    description: "Ruby",
+    inventory_image: "ruby.png",
+});
