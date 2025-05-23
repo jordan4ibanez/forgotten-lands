25ac6df6d7013b0b7b2e98a7254ceeb966515a72
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2025-05-22 21:06:15 -0400

    all that for a 🌳

--- source/crafter/nodes.ts
+++ source/crafter/nodes.ts
@@ -359,83 +359,124 @@ const acceptable_soil: { [id: string]: boolean } = {
 };
 
 core.register_node("main:tree", {
-    description : "Tree",
-    tiles : ["treeCore.png","treeCore.png","treeOut.png","treeOut.png","treeOut.png","treeOut.png"],
-    groups : {wood : 1, tree : 1, pathable : 1, flammable:1},
-    sounds : main.woodSound(),
-    // Set metadata so treecapitator doesn't destroy houses
-    on_place : (itemstack: ItemStackObject, placer: ObjectRef, pointed_thing: PointedThing): ItemStackObject | null => {
-		if ( pointed_thing.type != "node") {
-			return null
-        }
+	description: "Tree",
+	tiles: [
+		"treeCore.png",
+		"treeCore.png",
+		"treeOut.png",
+		"treeOut.png",
+		"treeOut.png",
+		"treeOut.png",
+	],
+	groups: { wood: 1, tree: 1, pathable: 1, flammable: 1 },
+	sounds: main.woodSound(),
+	// Set metadata so treecapitator doesn't destroy houses
+	on_place: (
+		itemstack: ItemStackObject,
+		placer: ObjectRef,
+		pointed_thing: PointedThing
+	): ItemStackObject | null => {
+		if (pointed_thing.type != "node") {
+			return null;
+		}
 
-		const sneak:boolean = placer.get_player_control().sneak
-		const noddef: NodeDefinition = core.registered_nodes[core.get_node(pointed_thing.under).name]
+		const sneak: boolean = placer.get_player_control().sneak;
+		const noddef: NodeDefinition =
+			core.registered_nodes[core.get_node(pointed_thing.under).name];
 
-		if (! sneak && noddef.on_rightclick) {
-			core.item_place(itemstack, placer, pointed_thing)
-			return null
-        }
+		if (!sneak && noddef.on_rightclick) {
+			core.item_place(itemstack, placer, pointed_thing);
+			return null;
+		}
 
-		const pos: Vec3 = pointed_thing.above
-		core.item_place_node(itemstack, placer, pointed_thing)
-		const meta: MetaRef = core.get_meta(pos)
-		meta.set_string("placed", "true")
-		return itemstack
-    },
+		const pos: Vec3 = pointed_thing.above;
+		core.item_place_node(itemstack, placer, pointed_thing);
+		const meta: MetaRef = core.get_meta(pos);
+		meta.set_string("placed", "true");
+		return itemstack;
+	},
 	// todo: treecapitator - move treecapitator into own file using override.
-	on_dig = function(pos, node, digger)
-		--bvav_create_vessel(pos,core.facedir_to_dir(core.dir_to_facedir(core.yaw_to_dir(digger:get_look_horizontal()+(math.pi/2)))))
-		--check if wielding axe?
-		--turn treecapitator into an enchantment?
-		local meta = core.get_meta(pos)
-		--local tool_meta = digger:get_wielded_item():get_meta()
-		--if tool_meta:get_int("treecapitator") > 0 then
-		if not meta:contains("placed") and string.match(digger:get_wielded_item():get_name(), "axe") then
-			local tool_capabilities = digger:get_wielded_item():get_tool_capabilities()
+	on_dig: (pos: Vec3, node: MapNode, digger: ObjectRef) => {
+		//bvav_create_vessel(pos,core.facedir_to_dir(core.dir_to_facedir(core.yaw_to_dir(digger:get_look_horizontal()+(math.pi/2)))))
+		//check if wielding axe?
+		//turn treecapitator into an enchantment?
+		const meta: MetaRef = core.get_meta(pos);
+		//local tool_meta = digger:get_wielded_item():get_meta()
+		//if tool_meta:get_int("treecapitator") > 0 then
+		if (
+			!meta.contains("placed") &&
+			string.match(digger.get_wielded_item().get_name(), "axe")
+		) {
+			const tool_capabilities: ToolCapabilities = digger
+				.get_wielded_item()
+				.get_tool_capabilities();
 
-			local wear = core.get_dig_params({wood=1}, tool_capabilities).wear
+			const wear: number = core.get_dig_params(
+				{ wood: 1 },
+				tool_capabilities
+			).wear;
 
-			local wield_stack = digger:get_wielded_item()
+			const wield_stack: ItemStackObject = digger.get_wielded_item();
 
-			--remove tree
-			for y = -6,6 do
-				local name = core.get_node(vector.new(pos.x,pos.y+y,pos.z)).name
+			//remove tree
+			for (let y = -6; y <= 6; y++) {
+				const name: string = core.get_node(
+					vector.create3d(pos.x, pos.y + y, pos.z)
+				).name;
 
-				if name == "main:tree" or name == "redstone:node_activated_tree" then
-					wield_stack:add_wear(wear)
-					core.node_dig(vector.new(pos.x,pos.y+y,pos.z), node, digger)
+				if (
+					name == "main:tree" ||
+					name == "redstone:node_activated_tree"
+				) {
+					wield_stack.add_wear(wear);
+					core.node_dig(
+						vector.create3d(pos.x, pos.y + y, pos.z),
+						node,
+						digger
+					);
 					core.add_particlespawner({
-						amount = 30,
-						time = 0.0001,
-						minpos = {x=pos.x-0.5, y=pos.y-0.5+y, z=pos.z-0.5},
-						maxpos = {x=pos.x+0.5, y=pos.y+0.5+y, z=pos.z+0.5},
-						minvel = vector.new(-1,0,-1),
-						maxvel = vector.new(1,0,1),
-						minacc = {x=0, y=-9.81, z=0},
-						maxacc = {x=0, y=-9.81, z=0},
-						minexptime = 0.5,
-						maxexptime = 1.5,
-						minsize = 0,
-						maxsize = 0,
-						collisiondetection = true,
-						vertical = false,
-						node = {name= name},
-					})
+						amount: 30,
+						time: 0.0001,
+						minpos: vector.create3d({
+							x: pos.x - 0.5,
+							y: pos.y - 0.5 + y,
+							z: pos.z - 0.5,
+						}),
+						maxpos: vector.create3d({
+							x: pos.x + 0.5,
+							y: pos.y + 0.5 + y,
+							z: pos.z + 0.5,
+						}),
+						minvel: vector.create3d(-1, 0, -1),
+						maxvel: vector.create3d(1, 0, 1),
+						minacc: vector.create3d({ x: 0, y: -9.81, z: 0 }),
+						maxacc: vector.create3d({ x: 0, y: -9.81, z: 0 }),
+						minexptime: 0.5,
+						maxexptime: 1.5,
+						minsize: 0,
+						maxsize: 0,
+						collisiondetection: true,
+						vertical: false,
+						node: { name: name },
+					});
 
-					local name2 = core.get_node(vector.new(pos.x,pos.y+y-1,pos.z)).name
-					if acceptable_soil[name2] then
-						core.add_node(vector.new(pos.x,pos.y+y,pos.z),{name="main:sapling"})
-					end
-				end
-			end
-			digger:set_wielded_item(wield_stack)
-		else
-			core.node_dig(pos, node, digger)
-		end
-
-	end
-})
+					const name2: string = core.get_node(
+						vector.create3d(pos.x, pos.y + y - 1, pos.z)
+					).name;
+					if (acceptable_soil[name2]) {
+						core.add_node(
+							vector.create3d(pos.x, pos.y + y, pos.z),
+							{ name: "main:sapling" }
+						);
+					}
+				}
+			}
+			digger.set_wielded_item(wield_stack);
+		} else {
+			core.node_dig(pos, node, digger);
+		}
+	},
+});
 
 // core.register_node("main:wood", {
 //     description = "Wood",
