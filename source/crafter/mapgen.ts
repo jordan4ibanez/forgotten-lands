4adc9e08392ea4435dd2fbad40e3fe09a4eb787a
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2025-05-22 23:36:40 -0400

    crafter

--- source/crafter/mapgen.ts
+++ source/crafter/mapgen.ts
@@ -1,19 +1,19 @@
-core.register_alias("mapgen_stone", "main:stone")
-core.register_alias("mapgen_dirt", "main:dirt")
-core.register_alias("mapgen_dirt_with_grass", "main:grass")
-core.register_alias("mapgen_water_source", "main:water")
-core.register_alias("mapgen_river_water_source", "main:water")
-core.register_alias("mapgen_sand", "main:sand")
-core.register_alias("mapgen_tree", "main:tree")
-core.register_alias("mapgen_leaves", "main:leaves")
-core.register_alias("mapgen_apple", "main:leaves")
+core.register_alias("mapgen_stone", "crafter:stone")
+core.register_alias("mapgen_dirt", "crafter:dirt")
+core.register_alias("mapgen_dirt_with_grass", "crafter:grass")
+core.register_alias("mapgen_water_source", "crafter:water")
+core.register_alias("mapgen_river_water_source", "crafter:water")
+core.register_alias("mapgen_sand", "crafter:sand")
+core.register_alias("mapgen_tree", "crafter:tree")
+core.register_alias("mapgen_leaves", "crafter:leaves")
+core.register_alias("mapgen_apple", "crafter:leaves")
 
 
 // core.register_biome({
 // 		name : "greenhills",
 // 		node_top : " main:grass",
 // 		depth_top : 1,
-// 		node_filler : "main:grass",
+// 		node_filler : "crafter:grass",
 // 		depth_filler : 3,
 // 		--node_riverbed : "default:sand",
 // 		--depth_riverbed : 2,
@@ -29,14 +29,14 @@ core.register_alias("mapgen_apple", "main:leaves")
 
 core.register_biome({
 	name : "grassland",
-	node_top : "main:grass",
+	node_top : "crafter:grass",
 	depth_top : 1,
-	node_filler : "main:dirt",
+	node_filler : "crafter:dirt",
 	depth_filler : 3,
-	node_riverbed : "main:dirt",
+	node_riverbed : "crafter:dirt",
 	depth_riverbed : 2,
-	node_dungeon : "main:cobble",
-	node_dungeon_alt : "main:mossy_cobble",
+	node_dungeon : "crafter:cobble",
+	node_dungeon_alt : "crafter:mossy_cobble",
 	node_dungeon_stair : "stairs:mossy_cobble_stair",
 	y_max : 256,
 	y_min : 6,
@@ -50,13 +50,13 @@ core.register_biome({
 // 		node_dust : "weather:snow",
 // 		node_top : "weather:snow_block",
 // 		depth_top : 4,
-// 		node_stone : "main:ice_mapgen",
-// 		node_filler : "main:dirt",
+// 		node_stone : "crafter:ice_mapgen",
+// 		node_filler : "crafter:dirt",
 // 		depth_filler : 0,
-// 		node_riverbed : "main:sand",
+// 		node_riverbed : "crafter:sand",
 // 		depth_riverbed : 2,
-// 		node_dungeon : "main:cobble",
-// 		node_dungeon_alt : "main:mossy_cobble",
+// 		node_dungeon : "crafter:cobble",
+// 		node_dungeon_alt : "crafter:mossy_cobble",
 // 		node_dungeon_stair : "stairs:mossy_cobble_stair",
 // 		y_max : 31000,
 // 		y_min : 4,
@@ -66,18 +66,18 @@ core.register_biome({
 
 // core.register_biome({
 // 		name : "snowcean",
-// 		node_dust : "main:snow",
-// 		node_top : "main:sand",
+// 		node_dust : "crafter:snow",
+// 		node_top : "crafter:sand",
 // 		depth_top : 1,
-// 		node_stone : "main:ice_mapgen",
-// 		node_filler : "main:sand",
+// 		node_stone : "crafter:ice_mapgen",
+// 		node_filler : "crafter:sand",
 // 		depth_filler : 3,
-// 		node_riverbed : "main:sand",
+// 		node_riverbed : "crafter:sand",
 // 		depth_riverbed : 2,
-// 		node_cave_liquid : "main:water",
-// 		node_dungeon : "main:cobble",
-// 		node_dungeon_alt : "main:mossy_cobble",
-// 		node_dungeon_stair : "main:mossy_cobble_stair",
+// 		node_cave_liquid : "crafter:water",
+// 		node_dungeon : "crafter:cobble",
+// 		node_dungeon_alt : "crafter:mossy_cobble",
+// 		node_dungeon_stair : "crafter:mossy_cobble_stair",
 // 		vertical_blend : 1,
 // 		y_max : 3,
 // 		y_min : -255,
@@ -91,13 +91,13 @@ core.register_biome({
 // 		depth_top : 1,
 // 		node_filler : "weather:snow_block",
 // 		depth_filler : 3,
-// 		node_stone : "main:ice_mapgen",
-// 		node_water_top : "main:ice_mapgen",
+// 		node_stone : "crafter:ice_mapgen",
+// 		node_water_top : "crafter:ice_mapgen",
 // 		depth_water_top : 10,
-// 		node_river_water : "main:ice_mapgen",
-// 		node_riverbed : "main:gravel",
+// 		node_river_water : "crafter:ice_mapgen",
+// 		node_riverbed : "crafter:gravel",
 // 		depth_riverbed : 2,
-// 		node_dungeon : "main:ice_mapgen",
+// 		node_dungeon : "crafter:ice_mapgen",
 // 		node_dungeon_stair : "stairs:glass_stair",
 // 		y_max : 31000,
 // 		y_min : -8,
@@ -107,14 +107,14 @@ core.register_biome({
 
 core.register_biome({
 	name : "sandDunes",
-	node_top : "main:sand",
+	node_top : "crafter:sand",
 	depth_top : 1,
-	node_filler : "main:sand",
+	node_filler : "crafter:sand",
 	depth_filler : 2,
-	node_riverbed : "main:sand",
+	node_riverbed : "crafter:sand",
 	depth_riverbed : 2,
-	node_dungeon : "main:cobble",
-	node_dungeon_alt : "main:mossy_cobble",
+	node_dungeon : "crafter:cobble",
+	node_dungeon_alt : "crafter:mossy_cobble",
 	node_dungeon_stair : "stairs:mossy_cobble_stair",
 	vertical_blend : 1,
 	y_max : 5,
@@ -125,15 +125,15 @@ core.register_biome({
 
 core.register_biome({
 	name : "beach",
-	node_top : "main:sand",
+	node_top : "crafter:sand",
 	depth_top : 1,
-	node_filler : "main:sand",
+	node_filler : "crafter:sand",
 	depth_filler : 3,
-	node_riverbed : "main:sand",
+	node_riverbed : "crafter:sand",
 	depth_riverbed : 2,
-	node_cave_liquid : "main:water",
-	node_dungeon : "main:cobble",
-	node_dungeon_alt : "main:mossy_cobble",
+	node_cave_liquid : "crafter:water",
+	node_dungeon : "crafter:cobble",
+	node_dungeon_alt : "crafter:mossy_cobble",
 	node_dungeon_stair : "stairs:mossy_cobble_stair",
 	y_max : 3,
 	y_min : -255,
@@ -143,9 +143,9 @@ core.register_biome({
 
 
 // core.register_decoration({
-// 	name : "main:tree",
+// 	name : "crafter:tree",
 // 	deco_type : "simple",
-// 	place_on : {"main:grass"},
+// 	place_on : {"crafter:grass"},
 // 	sidelen : 16,
 // 	fill_ratio : 0.005,
 // 	biomes : {"grassland"},
@@ -154,16 +154,16 @@ core.register_biome({
 // 	--schematic : treeSchematic,
 // 	--flags : "place_center_x, place_center_z, force_placement",
 // 	--rotation : "random",
-// 	decoration : "main:tree",
+// 	decoration : "crafter:tree",
 // 	height : 4,
 // 	height_max : 6,
 // })
 
 
 core.register_decoration({
-	name : "main:tree_big",
+	name : "crafter:tree_big",
 	deco_type : DecorationType.schematic,
-	place_on : ["main:grass"],
+	place_on : ["crafter:grass"],
 	sidelen : 16,
 	fill_ratio : 0.0025,
 	biomes : ["grassland"],
@@ -177,9 +177,9 @@ core.register_decoration({
 })
 
 core.register_decoration({
-	name : "main:tree_small",
+	name : "crafter:tree_small",
 	deco_type : DecorationType.schematic,
-	place_on : ["main:grass"],
+	place_on : ["crafter:grass"],
 	sidelen : 16,
 	fill_ratio : 0.0025,
 	biomes : ["grassland"],
