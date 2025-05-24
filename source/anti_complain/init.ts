456d772e56088a0c022c717637e00323d9b117ab
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2025-05-24 14:28:53 -0400

    Update init.ts

--- source/anti_complain/init.ts
+++ source/anti_complain/init.ts
@@ -1,6 +1,6 @@
 // Stop minetest from COMPLAINING ABOUT EVERYTHING!
 core.global_exists = (name: string): boolean => {
-    return true;
+	return true;
 };
 // This probably won't blow everything up, maybe.
 setmetatable(_G, {});
