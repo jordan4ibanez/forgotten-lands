b615f26d6b473f6897c7d3e5b5523a636a4e6916
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2025-05-24 13:57:37 -0400

    Update tools.ts

--- source/crafter/tools.ts
+++ source/crafter/tools.ts
@@ -1408,47 +1408,47 @@ namespace crafter {
 				}
 			}
 
-			// 		minetest.register_tool("main:"..material..tool, {
-			// 			description = material:gsub("^%l", string.upper).." "..tool:gsub("^%l", string.upper),
-			// 			inventory_image = material..tool..".png",
-			// 			tool_capabilities = {
-			// 				full_punch_interval = 0,
-			// 				//max_drop_level=0,
-			// 				groupcaps=groupcaps2,
-			// 				damage_groups = {damage=damage},
-			// 			},
-			// 			sound = {breaks = {name="tool_break",gain=0.4}}, // change this //todo: figure out what to change this to lol
-			// 			groups = {flammable = 2, tool=1 },
-			// 			mob_hit_wear = wear,
-			// 			//torch rightclick - hacked in since api doesn't call on_place correctly
-			// 			on_place = function(itemstack, placer, pointed_thing)
-			// 				local inv = placer:get_inventory()
-			// 				local torch = inv:contains_item("main", "torch:torch")
-			// 				local is_air = minetest.get_node(pointed_thing.above).name == "air"
-			// 				local dir = vector.subtract(pointed_thing.under, pointed_thing.above)
-			// 				local diff = dir.y
-			// 				local noddef = minetest.registered_nodes[minetest.get_node(pointed_thing.under).name]
-			// 				local walkable = noddef.walkable
-			// 				local sneak = placer:get_player_control().sneak
-			// 				if not sneak and noddef.on_rightclick then
-			// 					minetest.item_place(itemstack, placer, pointed_thing)
-			// 					return
-			// 				end
-			// 				if torch and is_air and walkable then
-			// 					if diff == 0 then
-			// 						local param2 = minetest.dir_to_wallmounted(dir)
-			// 						minetest.set_node(pointed_thing.above, {name="torch:wall",param2=param2})
-			// 						minetest.sound_play("wood", {pos=pointed_thing.above, gain = 1.0})
-			// 					elseif diff == -1 then
-			// 						minetest.place_node(pointed_thing.above,{name="torch:floor"})
-			// 					end
-			// 					//take item
-			// 					if diff == 0 or diff == -1 then
-			// 						inv:remove_item("main", "torch:torch")
-			// 					end
-			// 				end
-			// 			end,
-			// 		})
+					minetest.register_tool("main:"..material..tool, {
+						description = material:gsub("^%l", string.upper).." "..tool:gsub("^%l", string.upper),
+						inventory_image = material..tool..".png",
+						tool_capabilities = {
+							full_punch_interval = 0,
+							//max_drop_level=0,
+							groupcaps=groupcaps2,
+							damage_groups = {damage=damage},
+						},
+						sound = {breaks = {name="tool_break",gain=0.4}}, // change this //todo: figure out what to change this to lol
+						groups = {flammable = 2, tool=1 },
+						mob_hit_wear = wear,
+						//torch rightclick - hacked in since api doesn't call on_place correctly
+						on_place = function(itemstack, placer, pointed_thing)
+							local inv = placer:get_inventory()
+							local torch = inv:contains_item("main", "torch:torch")
+							local is_air = minetest.get_node(pointed_thing.above).name == "air"
+							local dir = vector.subtract(pointed_thing.under, pointed_thing.above)
+							local diff = dir.y
+							local noddef = minetest.registered_nodes[minetest.get_node(pointed_thing.under).name]
+							local walkable = noddef.walkable
+							local sneak = placer:get_player_control().sneak
+							if not sneak and noddef.on_rightclick then
+								minetest.item_place(itemstack, placer, pointed_thing)
+								return
+							end
+							if torch and is_air and walkable then
+								if diff == 0 then
+									local param2 = minetest.dir_to_wallmounted(dir)
+									minetest.set_node(pointed_thing.above, {name="torch:wall",param2=param2})
+									minetest.sound_play("wood", {pos=pointed_thing.above, gain = 1.0})
+								elseif diff == -1 then
+									minetest.place_node(pointed_thing.above,{name="torch:floor"})
+								end
+								//take item
+								if diff == 0 or diff == -1 then
+									inv:remove_item("main", "torch:torch")
+								end
+							end
+						end,
+					})
 		}
 
 		// 	local wear
