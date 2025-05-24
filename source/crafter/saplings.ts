bafb8dfdfbc7fc4fe31cf50edb3ba90c9b3cb76e
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2025-05-23 22:28:39 -0400

    Update saplings.ts

--- source/crafter/saplings.ts
+++ source/crafter/saplings.ts
@@ -6,109 +6,126 @@
 //make sapling grow
 
 function sapling_grow(pos: Vec3): void {
-    {
-    const light: number | null = core.get_node_light(pos, null);
-	if (light && light < 10) {
-		//print("failed to grow at "..dump(pos))
-		return
-    }
-    }
+	{
+		const light: number | null = core.get_node_light(pos, null);
+		if (light && light < 10) {
+			//print("failed to grow at "..dump(pos))
+			return;
+		}
+	}
 	//print("growing at "..dump(pos))
-    // todo: invert this logic
-	if (core.get_item_group(core.get_node(vector.create3d(pos.x,pos.y-1,pos.z)).name, "soil") > 0) {
-		let good_to_grow: boolean = true
+	// todo: invert this logic
+	if (
+		core.get_item_group(
+			core.get_node(vector.create3d(pos.x, pos.y - 1, pos.z)).name,
+			"soil"
+		) > 0
+	) {
+		let good_to_grow: boolean = true;
 		//check if room to grow (leaves or air)
 		for (let i = 1; i <= 4; i++) {
-			const node_name: string = core.get_node(vector.create3d(pos.x,pos.y+i,pos.z)).name
+			const node_name: string = core.get_node(
+				vector.create3d(pos.x, pos.y + i, pos.z)
+			).name;
 			if (node_name != "air" && node_name != "main:leaves") {
-				good_to_grow = false
-            }
-        }
+				good_to_grow = false;
+			}
+		}
 		if (good_to_grow) {
-			core.set_node(pos,{name:"main:tree"})
+			core.set_node(pos, { name: "main:tree" });
 
-			const schemmy: number = math.random(1,2)
+			const schemmy: number = math.random(1, 2);
 
-            // todo: this was using a schematic in the global namespace.
+			// todo: this was using a schematic in the global namespace.
 			// if (schemmy == 1) {
 			// 	core.place_schematic(pos, tree_big,"0",null,false,"place_center_x, place_center_z")
-            // }else if (schemmy == 2) {
+			// }else if (schemmy == 2) {
 			// 	core.place_schematic(pos, tree_small,"0",null,false,"place_center_x, place_center_z")
-            // }
+			// }
 
 			//override leaves
-			let max: number = 3
+			let max: number = 3;
 			if (schemmy == 2) {
-				max = 1
-            }
+				max = 1;
+			}
 			for (let i = 1; i <= max; i++) {
-				core.set_node(vector.create3d(pos.x,pos.y+i,pos.z),{name:"main:tree"})
-            }
-        }
-    }
+				core.set_node(vector.create3d(pos.x, pos.y + i, pos.z), {
+					name: "main:tree",
+				});
+			}
+		}
+	}
 }
 
 core.register_node("main:sapling", {
-	description = "Sapling",
-	drawtype = "plantlike",
-	inventory_image = "sapling.png",
-	waving = 1,
-	walkable = false,
-	climbable = false,
-	paramtype = "light",
-	is_ground_content = false,	
-	tiles = {"sapling.png"},
-	groups = {leaves = 1, plant = 1, axe = 1, hand = 0,instant=1, sapling=1, attached_node=1,flammable=1},
-	sounds = main.dirtSound(),
-	drop = "main:sapling",
-	node_placement_prediction = "",
-	selection_box = {
-		type = "fixed",
-		fixed = {-4 / 16, -0.5, -4 / 16, 4 / 16, 7 / 16, 4 / 16}
+	description: "Sapling",
+	drawtype: Drawtype.plantlike,
+	inventory_image: "sapling.png",
+	waving: 1,
+	walkable: false,
+	climbable: false,
+	paramtype: ParamType1.light,
+	is_ground_content: false,
+	tiles: ["sapling.png"],
+	groups: {
+		leaves: 1,
+		plant: 1,
+		axe: 1,
+		hand: 0,
+		instant: 1,
+		sapling: 1,
+		attached_node: 1,
+		flammable: 1,
 	},
-	on_place =  function(itemstack, placer, pointed_thing)
-		if not pointed_thing.type == "node" then
-			return
-		end
-		
-		local sneak = placer:get_player_control().sneak
-		local noddef = core.registered_nodes[core.get_node(pointed_thing.under).name]
-		
-		if not sneak and noddef.on_rightclick then
-			core.item_place(itemstack, placer, pointed_thing)
-			return
-		end
-		
-		local buildable = core.registered_nodes[core.get_node(pointed_thing.under).name].buildable_to
-		//replace buildable
-		if buildable and core.get_item_group(core.get_node(vector.new(pointed_thing.under.x,pointed_thing.under.y-1,pointed_thing.under.z)).name, "soil") > 0 then
-			return(core.item_place(itemstack, placer, pointed_thing))
-		end
-		local buildable = core.registered_nodes[core.get_node(pointed_thing.above).name].buildable_to
-		if buildable and core.get_item_group(core.get_node(vector.new(pointed_thing.above.x,pointed_thing.above.y-1,pointed_thing.above.z)).name, "soil") > 0 then
-			return(core.item_place(itemstack, placer, pointed_thing))
-		end
-		//place sapling
-		local pos = pointed_thing.above
-		if core.get_item_group(core.get_node(vector.new(pos.x,pos.y-1,pos.z)).name, "soil") > 0 and core.get_node(pointed_thing.above).name == "air" then
-			core.set_node(pointed_thing.above, {name="main:sapling"})
-			core.sound_play("dirt",{pos=pointed_thing.above})
-			itemstack:take_item(1)
-			return(itemstack)
-		end
-	end,
-})
+	sounds: main.dirtSound(),
+	drop: "main:sapling",
+	node_placement_prediction: "",
+	selection_box: {
+		type: Nodeboxtype.fixed,
+		fixed: [-4 / 16, -0.5, -4 / 16, 4 / 16, 7 / 16, 4 / 16],
+	},
+	// on_place =  function(itemstack, placer, pointed_thing)
+	// 	if not pointed_thing.type == "node" then
+	// 		return
+	// 	end
 
+	// 	local sneak = placer:get_player_control().sneak
+	// 	local noddef = core.registered_nodes[core.get_node(pointed_thing.under).name]
+
+	// 	if not sneak and noddef.on_rightclick then
+	// 		core.item_place(itemstack, placer, pointed_thing)
+	// 		return
+	// 	end
+
+	// 	local buildable = core.registered_nodes[core.get_node(pointed_thing.under).name].buildable_to
+	// 	//replace buildable
+	// 	if buildable and core.get_item_group(core.get_node(vector.new(pointed_thing.under.x,pointed_thing.under.y-1,pointed_thing.under.z)).name, "soil") > 0 then
+	// 		return(core.item_place(itemstack, placer, pointed_thing))
+	// 	end
+	// 	local buildable = core.registered_nodes[core.get_node(pointed_thing.above).name].buildable_to
+	// 	if buildable and core.get_item_group(core.get_node(vector.new(pointed_thing.above.x,pointed_thing.above.y-1,pointed_thing.above.z)).name, "soil") > 0 then
+	// 		return(core.item_place(itemstack, placer, pointed_thing))
+	// 	end
+	// 	//place sapling
+	// 	local pos = pointed_thing.above
+	// 	if core.get_item_group(core.get_node(vector.new(pos.x,pos.y-1,pos.z)).name, "soil") > 0 and core.get_node(pointed_thing.above).name == "air" then
+	// 		core.set_node(pointed_thing.above, {name="main:sapling"})
+	// 		core.sound_play("dirt",{pos=pointed_thing.above})
+	// 		itemstack:take_item(1)
+	// 		return(itemstack)
+	// 	end
+	// end,
+});
 
 //growing abm for sapling
-core.register_abm({
-	label = "Tree Grow",
-	nodenames = {"group:sapling"},
-	neighbors = {"group:soil"},
-	interval = 6,
-	chance = 250,
-	catch_up = true,
-	action = function(pos)
-		sapling_grow(pos)
-	end,
-})
+// core.register_abm({
+// 	label = "Tree Grow",
+// 	nodenames = {"group:sapling"},
+// 	neighbors = {"group:soil"},
+// 	interval = 6,
+// 	chance = 250,
+// 	catch_up = true,
+// 	action = function(pos)
+// 		sapling_grow(pos)
+// 	end,
+// })
