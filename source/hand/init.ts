ec4a4ea06c76c6660435b6c0cde9bb394fabcf07
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2025-05-24 15:13:56 -0400

    Update init.ts

--- source/hand/init.ts
+++ source/hand/init.ts
@@ -105,46 +105,46 @@ core.register_node("crafter_hand:player", {
 	range: def.range,
 });
 
-// core.register_node("hand:creative", {
-// 	description = "",
-// 	tiles = {"player.png"},
-// 	visual_scale = 1,
-// 	tool_capabilities = {
-// 		full_punch_interval = 0.9,
-// 		max_drop_level = 0,
-// 		groupcaps = {
-// 			stone = {times={[1]=0,[2]=0,[3]=0,[4]=0,[5]=0}, uses=0, maxlevel=1},
-// 			dirt =  {times={[1]=0,[2]=0,[3]=0,[4]=0,[5]=0}, uses=0, maxlevel=1},
-// 			snow =  {times={[1]=0,[2]=0,[3]=0,[4]=0,[5]=0}, uses=0, maxlevel=1},
-// 			grass = {times={[1]=0,[2]=0,[3]=0,[4]=0,[5]=0}, uses=0, maxlevel=1},
-// 			sand =  {times={[1]=0,[2]=0,[3]=0,[4]=0,[5]=0}, uses=0, maxlevel=1},
-// 			wood =  {times={[1]=0,[2]=0,[3]=0,[4]=0,[5]=0}, uses=0, maxlevel=1},
-// 			leaves ={times={[1]=0,[2]=0,[3]=0,[4]=0,[5]=0}, uses=0, maxlevel=1},
-// 			wool =  {times={[1]=0,[2]=0,[3]=0,[4]=0,[5]=0}, uses=0, maxlevel=1},
-// 			glass = {times={[1]=0,[2]=0,[3]=0,[4]=0,[5]=0}, uses=0, maxlevel=1},
-// 			netherrack = {times={[1]=0,[2]=0,[3]=0,[4]=0,[5]=0},uses=0, maxlevel=1},
-// 			unbreakable = {times={[1]=0,[2]=0,[3]=0,[4]=0,[5]=0},uses=0, maxlevel=1},
-// 			dig_immediate = {times={[2]=0,[3]=0,[1]=0,},uses=0, maxlevel=1},
-// 		},
-// 		damage_groups = {damage=1},
-// 	},
-// 	wield_scale = {x=1,y=1,z=1},
-// 	paramtype = "light",
-// 	drawtype = "mesh",
-// 	mesh = "hand.b3d",
-// 	// Prevent construction
-// 	node_placement_prediction = "",
-// 	on_construct = function(pos)
-// 		core.log("error", "Tried to place hand at "..core.pos_to_string(pos))
-// 		core.remove_node(pos)
-// 	end,
-// 	drop = "",
-// 	on_drop = function()
-// 		return ""
-// 	end,
-// 	groups = { dig_immediate = 3, not_in_creative_inventory = 1 },
-// 	range = def.range,
-// })
+core.register_node("hand:creative", {
+	description : "",
+	tiles : ["player.png"],
+	visual_scale : 1,
+	tool_capabilities : {
+		full_punch_interval : 0.9,
+		max_drop_level : 0,
+		groupcaps : {
+			stone : {times:{[1]:0,[2]:0,[3]:0,[4]:0,[5]:0}, uses:0, maxlevel:1},
+			dirt :  {times:{[1]:0,[2]:0,[3]:0,[4]:0,[5]:0}, uses:0, maxlevel:1},
+			snow :  {times:{[1]:0,[2]:0,[3]:0,[4]:0,[5]:0}, uses:0, maxlevel:1},
+			grass : {times:{[1]:0,[2]:0,[3]:0,[4]:0,[5]:0}, uses:0, maxlevel:1},
+			sand :  {times:{[1]:0,[2]:0,[3]:0,[4]:0,[5]:0}, uses:0, maxlevel:1},
+			wood :  {times:{[1]:0,[2]:0,[3]:0,[4]:0,[5]:0}, uses:0, maxlevel:1},
+			leaves :{times:{[1]:0,[2]:0,[3]:0,[4]:0,[5]:0}, uses:0, maxlevel:1},
+			wool :  {times:{[1]:0,[2]:0,[3]:0,[4]:0,[5]:0}, uses:0, maxlevel:1},
+			glass : {times:{[1]:0,[2]:0,[3]:0,[4]:0,[5]:0}, uses:0, maxlevel:1},
+			netherrack : {times:{[1]:0,[2]:0,[3]:0,[4]:0,[5]:0},uses:0, maxlevel:1},
+			unbreakable : {times:{[1]:0,[2]:0,[3]:0,[4]:0,[5]:0},uses:0, maxlevel:1},
+			dig_immediate : {times:{[2]:0,[3]:0,[1]:0,},uses:0, maxlevel:1},
+		},
+		damage_groups : {damage:1},
+	},
+	wield_scale : vector.create3d({x:1,y:1,z:1}),
+	paramtype : ParamType1.light,
+	drawtype : Drawtype.mesh,
+	mesh : "hand.b3d",
+	// Prevent construction
+	node_placement_prediction : "",
+	on_construct : (pos: Vec3) => {
+		core.log(LogLevel.error, "Tried to place hand at "+core.pos_to_string(pos))
+		core.remove_node(pos)
+    },
+	drop : "",
+	on_drop : () =>{
+		return ""
+    },
+	groups : { dig_immediate : 3, not_in_creative_inventory : 1 },
+	range : def.range,
+})
 
 //Create a hand list and then enable the hand node
 core.register_on_joinplayer((player: ObjectRef) => {
