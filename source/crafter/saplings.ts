d8bf7a7f1014bc85f53af5b87aff1b829abd17ee
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2025-05-23 22:20:21 -0400

    Update saplings.ts

--- source/crafter/saplings.ts
+++ source/crafter/saplings.ts
@@ -6,27 +6,27 @@
 //make sapling grow
 
 function sapling_grow(pos: Vec3): void {
-	if minetest.get_node_light(pos, nil) < 10 then
+	if core.get_node_light(pos, nil) < 10 then
 		//print("failed to grow at "..dump(pos))
 		return
 	end
 	//print("growing at "..dump(pos))
-	if minetest.get_item_group(minetest.get_node(vector.new(pos.x,pos.y-1,pos.z)).name, "soil") > 0 then
+	if core.get_item_group(core.get_node(vector.new(pos.x,pos.y-1,pos.z)).name, "soil") > 0 then
 		local good_to_grow = true
 		//check if room to grow (leaves or air)
 		for i = 1,4 do
-			local node_name = minetest.get_node(vector.new(pos.x,pos.y+i,pos.z)).name
+			local node_name = core.get_node(vector.new(pos.x,pos.y+i,pos.z)).name
 			if node_name ~= "air" and node_name ~= "main:leaves" then
 				good_to_grow = false
 			end
 		end
 		if good_to_grow == true then
-			minetest.set_node(pos,{name="main:tree"})
+			core.set_node(pos,{name="main:tree"})
 			local schemmy = math.random(1,2)
 			if schemmy == 1 then
-				minetest.place_schematic(pos, tree_big,"0",nil,false,"place_center_x, place_center_z")
+				core.place_schematic(pos, tree_big,"0",nil,false,"place_center_x, place_center_z")
 			elseif schemmy == 2 then
-				minetest.place_schematic(pos, tree_small,"0",nil,false,"place_center_x, place_center_z")
+				core.place_schematic(pos, tree_small,"0",nil,false,"place_center_x, place_center_z")
 			end
 			//override leaves
 			local max = 3
@@ -34,13 +34,13 @@ function sapling_grow(pos: Vec3): void {
 				max = 1
 			end
 			for i = 1,max do
-				minetest.set_node(vector.new(pos.x,pos.y+i,pos.z),{name="main:tree"})
+				core.set_node(vector.new(pos.x,pos.y+i,pos.z),{name="main:tree"})
 			end
 		end
 	end
 }
 
-minetest.register_node("main:sapling", {
+core.register_node("main:sapling", {
 	description = "Sapling",
 	drawtype = "plantlike",
 	inventory_image = "sapling.png",
@@ -64,27 +64,27 @@ minetest.register_node("main:sapling", {
 		end
 		
 		local sneak = placer:get_player_control().sneak
-		local noddef = minetest.registered_nodes[minetest.get_node(pointed_thing.under).name]
+		local noddef = core.registered_nodes[core.get_node(pointed_thing.under).name]
 		
 		if not sneak and noddef.on_rightclick then
-			minetest.item_place(itemstack, placer, pointed_thing)
+			core.item_place(itemstack, placer, pointed_thing)
 			return
 		end
 		
-		local buildable = minetest.registered_nodes[minetest.get_node(pointed_thing.under).name].buildable_to
+		local buildable = core.registered_nodes[core.get_node(pointed_thing.under).name].buildable_to
 		//replace buildable
-		if buildable and minetest.get_item_group(minetest.get_node(vector.new(pointed_thing.under.x,pointed_thing.under.y-1,pointed_thing.under.z)).name, "soil") > 0 then
-			return(minetest.item_place(itemstack, placer, pointed_thing))
+		if buildable and core.get_item_group(core.get_node(vector.new(pointed_thing.under.x,pointed_thing.under.y-1,pointed_thing.under.z)).name, "soil") > 0 then
+			return(core.item_place(itemstack, placer, pointed_thing))
 		end
-		local buildable = minetest.registered_nodes[minetest.get_node(pointed_thing.above).name].buildable_to
-		if buildable and minetest.get_item_group(minetest.get_node(vector.new(pointed_thing.above.x,pointed_thing.above.y-1,pointed_thing.above.z)).name, "soil") > 0 then
-			return(minetest.item_place(itemstack, placer, pointed_thing))
+		local buildable = core.registered_nodes[core.get_node(pointed_thing.above).name].buildable_to
+		if buildable and core.get_item_group(core.get_node(vector.new(pointed_thing.above.x,pointed_thing.above.y-1,pointed_thing.above.z)).name, "soil") > 0 then
+			return(core.item_place(itemstack, placer, pointed_thing))
 		end
 		//place sapling
 		local pos = pointed_thing.above
-		if minetest.get_item_group(minetest.get_node(vector.new(pos.x,pos.y-1,pos.z)).name, "soil") > 0 and minetest.get_node(pointed_thing.above).name == "air" then
-			minetest.set_node(pointed_thing.above, {name="main:sapling"})
-			minetest.sound_play("dirt",{pos=pointed_thing.above})
+		if core.get_item_group(core.get_node(vector.new(pos.x,pos.y-1,pos.z)).name, "soil") > 0 and core.get_node(pointed_thing.above).name == "air" then
+			core.set_node(pointed_thing.above, {name="main:sapling"})
+			core.sound_play("dirt",{pos=pointed_thing.above})
 			itemstack:take_item(1)
 			return(itemstack)
 		end
@@ -93,7 +93,7 @@ minetest.register_node("main:sapling", {
 
 
 //growing abm for sapling
-minetest.register_abm({
+core.register_abm({
 	label = "Tree Grow",
 	nodenames = {"group:sapling"},
 	neighbors = {"group:soil"},
